cc408b9969e8c9f6fa37aad33f37779e90cdca20
PatrykFlama <patrykflama@interia.pl>
2023-03-05 23:02:38 +0100

    C# - WHAAAAT?!

---
+++ Sem2/PO/Lista 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+namespace Program // Note: actual namespace depends on the project name.
+{
+    internal class BigNum
+    {
+        int num;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        void Add(BigNum a, BigNum b){
+            
+        }
+    }
+}
---
+++ Sem2/PO/Lista 2/README.md
@@ -0,0 +1,4 @@
+# How to use .NET / C#
+* initialize project with _dotnet new console_
+* _dotnet run_ to run
+* _dotnet restore_ to ???

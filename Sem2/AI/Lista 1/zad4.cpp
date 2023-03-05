fc2b64f4ae48395adb0b8a1c9ea54e414fc17f68
PatrykFlama <patrykflama@interia.pl>
2023-03-05 20:03:16 +0100

    AI 1/4

--- Sem2/AI/Lista 1/zad4.cpp
+++ Sem2/AI/Lista 1/zad4.cpp
@@ -11,15 +11,15 @@ int main(){
     int maxx = 0, max_ptr = 0;
     int number_of_bits = 0;
 
-    // TODO if n > str.size()
+    if(n > str.size()) return 0;
 
     // create prefix table which shows how many bits are on in previos n cells
     if(str[0] == '1') prefix[0] = 1;
-    for(int c = 0; c < n-1; c++) prefix[c+1] = prefix[c] + (str[c+1] == '1' ? 1 : 0);
-    number_of_bits = prefix[n-1];
+    for(int c = 1; c < n; c++) prefix[c] = prefix[c-1] + (str[c] == '1' ? 1 : 0);
+    if(n > 0) number_of_bits = prefix[n-1];
 
-    for(int c = n-1; c < str.size(); c++){
-        prefix[c] = prefix[c-1];
+    for(int c = n; c < str.size(); c++){
+        if(c > 0) prefix[c] = prefix[c-1];
         if(str[c-n] == '1') prefix[c]--;
         if(str[c] == '1') {
             ++number_of_bits;
@@ -35,7 +35,7 @@ int main(){
     int bits_to_switch = n - maxx;   // number of off bits in 'maximal on bits' range
     bits_to_switch += number_of_bits - maxx;         // bits out of 'max on bits' range
 
-    if(true){
+    if(false){
         for(int i = 0; i < str.size(); i++) cout << prefix[i];
         cout << '\n';
         cout << maxx << ' ' << max_ptr << ' ' << number_of_bits << '\n';

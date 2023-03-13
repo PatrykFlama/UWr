40d38943f6b597c82b978c93cc2327d16c390afb
PatrykFlama <patrykflama@interia.pl>
2023-03-13 15:33:20 +0100

    MP 3

--- Sem2/MP/Lista 3/zad.rkt
+++ Sem2/MP/Lista 3/zad.rkt
@@ -52,6 +52,32 @@
 
 ; Zadanie 7
 (define ( foldr-reverse xs )
-    (foldr ( lambda (y ys ) ( append ys ( list y))) null xs ))
-
+    (foldr ( lambda (y ys) ( append ys ( list y))) null xs ))
+; 
 (length (foldr-reverse (build-list 10000 identity)))
+
+; Zadanie 8
+; llist jest listą reprezentowaną przez funkcję
+; funckja ta pamięta jedną listę oraz po zaaplikowaniu do innej listy zwraca ich konkatencję
+(define (list->llist xs)
+  (lambda (x) (append xs x)))
+
+(define (llist->list f) (f null))
+
+(define llist-null (list->llist '()))
+
+(define (llist-singleton x)
+  (list->llist '(x)))
+
+(define (llist-append f g)
+  (lambda (x) (f (g x))))
+
+(define (foldr-llist-reverse xs)
+    (llist->list
+        (foldr
+            (lambda (y ys) (llist-append ys (list->llist '(y))))
+            llist-null
+            xs)))
+
+(length (foldr-reverse (build-list 40000 identity)))
+(length (foldr-llist-reverse (build-list 40000 identity)))

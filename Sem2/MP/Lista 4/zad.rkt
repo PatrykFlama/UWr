bb78860ab76a3ae6325fa83c25feeef945df4c1f
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-03-18 11:39:27 +0100

    MP wip

--- Sem2/MP/Lista 4/zad.rkt
+++ Sem2/MP/Lista 4/zad.rkt
@@ -3,7 +3,20 @@
 (define-struct leaf () #:transparent)
 (define-struct node (l elem r) #:transparent)
 
-(define (foldr f elem xs)
-    (if (leaf? xs) 
+(define (my-foldr f x xs)
+  (if (null? xs)
+      x
+      (f (car xs) (my-foldr f x (cdr xs)))))
+
+(define (my-foldl f x xs)
+  (define (it xs acc)
+    (if (null? xs)
+        acc
+        (it (cdr xs) (f (car xs) acc))))
+  (it xs x))
+
+
+(define (fold-tree f elem t)
+    (if (leaf? t)
         elem
-        (foldr f)))
+        (f (node-elem t) (fold-tree f elem) )))    

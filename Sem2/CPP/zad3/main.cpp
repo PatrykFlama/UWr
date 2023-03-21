d2a41ae65fcd2860821d2d7f9c751e6088c3569d
PatrykFlama <patrykflama@interia.pl>
2023-03-21 21:00:52 +0100

    cpp smth

--- Sem2/CPP/zad3/main.cpp
+++ Sem2/CPP/zad3/main.cpp
@@ -2,5 +2,14 @@
 
 
 int main(){
-    
+    Number a;
+    Number b(5);
+
+    b.insert(6);
+    Number c(b);
+    Number d;
+    b.insert(7);
+    d = b;
+
+    printf("a:%f b:%f c:%f d:%f\n", a, b, c, d);
 }

97e75aa36d148bb6f223533e98f3e4a6cff17974
PatrykFlama <patrykflama@interia.pl>
2023-03-21 10:08:29 +0100

    PO 4/3

--- Sem2/PO/Lista 4/zad3.cs
+++ Sem2/PO/Lista 4/zad3.cs
@@ -32,8 +32,8 @@ class GraphList : IGraph{
         get {return _edges;}
     }
 
-    public void append(string from, string to){
-        if(!graph.ContainsKey(from)) 
+    public void append(string from, string to){     // TODO - chech if vortex name already exists!
+        if(!graph.ContainsKey(from))
             graph.Add(from, new List<string>());
         if(!graph.ContainsKey(to)) 
             graph.Add(to, new List<string>());
@@ -51,11 +51,20 @@ class GraphList : IGraph{
         _edges--;
     }
 
-    public void print(){
-        foreach(KeyValuePair<string, List<string>> i in graph){
-            Console.Write(i.Key, ": ");
-            Console.WriteLine(string.Join(", ", i.Value));
+    public List<string> neighbours(string node_name){
+        if(!graph.ContainsKey(node_name)) return new List<string>();        // this vortex doesn't exist!
+
+        return graph[node_name];
+    }
+
+    public string print(){
+        string s = "";
+        foreach(KeyValuePair<string, List<string>> node in graph){
+            s += node.Key + " -> ";
+            s += string.Join(", ", node.Value);
+            s += "\n";
         }
+        return s;
     }
 };
 
@@ -64,23 +73,23 @@ class GraphMatrix : IGraph{
     int _edges;
     bool[,] matrix;
 
-    Dictionary<string, int> VertexId;
-    Dictionary<int, string> VertexLabel;
+    Dictionary<string, int> v_ptr;      // for finding verticies
+    Dictionary<int, string> v_name;     // for finding names
 
 
-    int GetId(string a){
-        return VertexId.ContainsKey(a) ? VertexId[a] : VertexId.Count;
+    int get_ptr(string a){
+        return v_ptr.ContainsKey(a) ? v_ptr[a] : v_ptr.Count;
     }
 
     public GraphMatrix(int n){
         _verticies = n;
         _edges = 0;
         matrix = new bool[n, n];
-        VertexId = new Dictionary<string, int>();
-        VertexLabel = new Dictionary<int, string>();
+        v_ptr = new Dictionary<string, int>();
+        v_name = new Dictionary<int, string>();
     }
 
-    public int SizeV{
+    public int verticies{
         get {return _verticies;}
     }
     public int edges{
@@ -88,52 +97,55 @@ class GraphMatrix : IGraph{
     }
 
     public void append(string from, string to){
-        int id_a = GetId(from);
-        if(id_a == VertexId.Count){
-            VertexId.Add(from,id_a);
-            VertexLabel.Add(id_a,from);
+        int ptr_from = get_ptr(from);
+        if(ptr_from == v_ptr.Count){
+            v_ptr.Add(from, ptr_from);
+            v_name.Add(ptr_from, from);
         }
 
-        int id_b = GetId(b);
-        if(id_b == VertexId.Count){
-            VertexId.Add(b,id_b);
-            VertexLabel.Add(id_b,b);
+        int ptr_to = get_ptr(to);
+        if(ptr_to == v_ptr.Count){
+            v_ptr.Add(to, ptr_to);
+            v_name.Add(ptr_to, to);
         }
 
-        matrix[id_a,id_b] = matrix[id_b,id_a] = true;
+        matrix[ptr_from, ptr_to] = matrix[ptr_to, ptr_from] = true;
         _edges++;
     }
 
-    public void RemoveEdge(string from, string b){
-        int id_a = GetId(from);
-        int id_b = GetId(b);
+    public void remove(string from, string to){
+        int ptr_from = get_ptr(from);
+        int ptr_to = get_ptr(to);
 
-        if(id_a == VertexId.Count || id_b == VertexId.Count) 
-            throw new Exception("Proba usuniecia nieistniejacej krawedzi!");
-        matrix[id_a,id_b] = matrix[id_b,id_a] = false;
+        if(ptr_from == v_ptr.Count || ptr_to == v_ptr.Count) return;        // vortex doesnt exist!
+
+        matrix[ptr_from, ptr_to] = matrix[ptr_to, ptr_from] = false;
         _edges--;
     }
 
-    public List<string> GetNeighbors(string s){
-        int x = GetId(s);
-        if(x == VertexId.Count) 
-            throw new Exception("Odwolanie do nieistniejacego wierzcholka!");
+    public List<string> neighbours(string name_v){
+        int ptr_v = get_ptr(name_v);
+        if(ptr_v == v_ptr.Count) return new List<string>();                 // vortex doesnt exist!
 
-        List<string> l = new List<string>();
-        for(int i = 0; i < size_v; i++)
-            if(matrix[x,i]) l.Add(VertexLabel[i]);
-        return l;
+        List<string> res = new List<string>();
+        for(int i = 0; i < _verticies; i++)
+            if(matrix[ptr_v, i]) 
+                res.Add(v_name[i]);
+
+        return res;
     }
     
-    public void print(){
-        for(int i = 0; i < size_v; i++)
-            if(VertexLabel.ContainsKey(i)){
-                Console.Write("{0}: ", VertexLabel[i]);
-                Console.WriteLine(string.Join(", ", GetNeighbors(VertexLabel[i])));
-            }
+    public string print(){
+        string res = "";
+        foreach(KeyValuePair<int, string> name in v_name){
+            res += name.Value + " -> ";
+            res += string.Join(", ", neighbours(name.Value));
+            res += "\n";
+        }
+        return res;
     }
 };
-
+/*
 class MyRandom : Random{
     public int RandomInt(){
         return Next();
@@ -173,9 +185,9 @@ class GraphOperations{
         while(q.Count > 0){
             string s = q.Dequeue();
             if(s == b) break;
-            List<string> neighbors = g.GetNeighbors(s);
+            List<string> neighbours = g.neighbours(s);
             
-            foreach(string x in neighbors)
+            foreach(string x in neighbours)
                 if(!visited.Contains(x)){
                     prev.Add(x,s);
                     visited.Add(x);
@@ -188,47 +200,56 @@ class GraphOperations{
         res.Reverse();
         return res;
     }
-}
+}*/
 
 
 class Program{
     public static void Main(){
-        GraphList G = new GraphList(10);
-        //GraphMatrix G = new GraphMatrix(10);
+        GraphList graph_l = new GraphList(10);
+        GraphMatrix graph_m = new GraphMatrix(10);
 
-        G.append("babcia","jadzia");
-        G.append("jadzia","basia");
-        G.append("basia","stasia");
-        G.append("stasia","jadzia");
-        Console.WriteLine(G.edges);
+        graph_m.append("a","b");
+        graph_m.append("b","a");
+        graph_m.append("c","a");
+        graph_m.append("a","w");
+        Console.WriteLine(graph_m.edges);
+        Console.WriteLine(graph_m.print());
+        
+        Console.WriteLine("---------------\n");
+        graph_l.append("a","b");
+        graph_l.append("b","a");
+        graph_l.append("c","a");
+        graph_l.append("a","w");
+        Console.WriteLine(graph_l.edges);
+        Console.WriteLine(graph_l.print());
 
-        G.print();
+        // G.print();
 
-        Console.WriteLine(string.Join(", ", G.GetNeighbors("babcia")));
-        Console.WriteLine(string.Join(", ", G.GetNeighbors("jadzia")));
-        Console.WriteLine(string.Join(", ", GraphOperations.ShortestPath(G,"babcia","stasia")));
-        Console.WriteLine(string.Join(", ", GraphOperations.ShortestPath(G,"basia","babcia")));
+        // Console.WriteLine(string.Join(", ", G.neighbours("babcia")));
+        // Console.WriteLine(string.Join(", ", G.neighbours("jadzia")));
+        // Console.WriteLine(string.Join(", ", GraphOperations.ShortestPath(G,"babcia","stasia")));
+        // Console.WriteLine(string.Join(", ", GraphOperations.ShortestPath(G,"basia","babcia")));
 
-        G.RemoveEdge("jadzia","stasia");
-        Console.WriteLine(string.Join(", ", GraphOperations.ShortestPath(G,"babcia","stasia")));
-        G.RemoveEdge("jadzia","basia");
-        Console.WriteLine(string.Join(", ", GraphOperations.ShortestPath(G,"basia","babcia")));
-        Console.WriteLine(string.Join(", ", GraphOperations.ShortestPath(G,"basia","basia")));
-        Console.WriteLine(G.edges);
+        // G.RemoveEdge("jadzia","stasia");
+        // Console.WriteLine(string.Join(", ", GraphOperations.ShortestPath(G,"babcia","stasia")));
+        // G.RemoveEdge("jadzia","basia");
+        // Console.WriteLine(string.Join(", ", GraphOperations.ShortestPath(G,"basia","babcia")));
+        // Console.WriteLine(string.Join(", ", GraphOperations.ShortestPath(G,"basia","basia")));
+        // Console.WriteLine(G.edges);
 
-        try{
-            G.RemoveEdge("kasia","babcia");
-        } catch(Exception e){
-            Console.WriteLine(e);
-        }
-        try{
-            G.GetNeighbors("dziadek");
-        } catch(Exception e){
-            Console.WriteLine(e);
-        }
+        // try{
+        //     G.RemoveEdge("kasia","babcia");
+        // } catch(Exception e){
+        //     Console.WriteLine(e);
+        // }
+        // try{
+        //     G.neighbours("dziadek");
+        // } catch(Exception e){
+        //     Console.WriteLine(e);
+        // }
 
-        G = (GraphList)GraphOperations.RandomGraph(G,8,12);
-        //G = (GraphMatrix)GraphOperations.RandomGraph(G,8,12);
-        G.print();
+        // G = (GraphList)GraphOperations.RandomGraph(G,8,12);
+        // // G = (GraphMatrix)GraphOperations.RandomGraph(G,8,12);
+        // G.print();
     }
 }

751aa036a4bdaa5d6faf42c3bba2340e397915a0
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-03-22 12:58:39 +0100

    PARO yup

---
+++ Sem2/PARO/zad2/exercises/build/resources/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/patry/Documents/Programming/UWr/Sem2/PARO/zad2/exercises/resources
+# Build directory: /mnt/c/Users/patry/Documents/Programming/UWr/Sem2/PARO/zad2/exercises/build/resources
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

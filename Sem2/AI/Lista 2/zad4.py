6eaa9f219d1044a54f851009555b520e3a2a74c6
PatrykFlama <patrykflama@interia.pl>
2023-03-25 14:37:42 +0100

    fdsajnkb

--- Sem2/AI/Lista 2/zad4.py
+++ Sem2/AI/Lista 2/zad4.py
@@ -44,7 +44,7 @@ def make_move(state, dir):
 
 dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-def make_random_moves(start_state, count):
+def make_random_moves(start_state, count = 50, commandos_alive = 5):
     last_random_move = -1
     for i in range(0, count):
         random_move = random.randint(0, 3)
@@ -53,6 +53,8 @@ def make_random_moves(start_state, count):
 
         start_state = make_move(start_state, dirs[random_move])
         last_random_move = random_move
+
+        # if len(start_state) <= commandos_alive: return start_state
     return start_state
 
 
@@ -61,27 +63,79 @@ def goal_reached(state):
         if commando not in board_goal: return False
     return True
 
-def bfs(start_state):
+def bfs(start_state, max_depth = 15):
     q = []
-    vis = set()
-    q.append(tuple(start_state))
-    print("Commandos:", len(start_state))
+    vis = []
+    q.append((start_state, 0))
+    max_len = len(start_state)
 
     while len(q) != 0:
         current_state = q.pop(0)
-        vis.add(current_state)
-        if goal_reached(current_state): return current_state
+        depth = current_state[1]
+        current_state = current_state[0]
 
+        vis.append(current_state)
+        if goal_reached(current_state): return current_state
+        if len(current_state) > max_len: continue
+        if depth > max_depth: continue
+
+        # print("\n", current_state, "from", q)
         for d in dirs:
-            new_state = frozenset(make_move(current_state, d))
-            if new_state not in vis:        # TODO that does not work # TODO basicly something is something and so something is not workign very well
-                q.append(tuple(new_state))
-    
+            new_state = make_move(current_state, d)
+            # print(new_state, "in?", vis, new_state in vis)
+            if new_state not in vis:
+                max_len = max(max_len, len(new_state))
+                q.append((new_state, depth+1))
+
+        # print_board(current_state)
+        # i = input()
+
     return False
+
+def print_board(state):
+    for row in range(0, len(board_walls)):
+        for col in range(0, len(board_walls[row])):
+            if board_walls[row][col]: print("#", end='')
+            elif (row, col) in state and (row, col) in board_goal: print("B", end='')
+            elif (row, col) in state: print("S", end='')
+            elif (row, col) in board_goal: print("G", end='')
+            else: print(" ", end='')
+
+        print()
 
 
 # --------- MAIN ---------
 read_board()
 print(board_goal)
 board_start = make_random_moves(board_start, 150)
+print_board(board_start)
+
 print(bfs(board_start))
+
+# for t in range(50, 131, 5):
+#     avg = 0
+#     for i in range(0, 10000):
+#         avg += len(make_random_moves(board_start, t))
+#         avg /= 2
+
+#     print(t, avg)
+
+"""
+50 7.504882128705937
+55 5.0261831877677725
+60 8.71381990697462
+65 5.041194659749196
+70 3.9756192866807853
+75 4.0198719750679714
+80 2.7986840969685396
+85 4.027713963916065
+90 2.9102440109080057
+95 4.013103733226822
+100 4.093308143976234
+105 3.5059002954630083
+110 4.091924189420891
+115 2.3464528164446543
+120 3.22358637019109
+125 3.2539674253346913
+130 2.5826009632955107
+"""

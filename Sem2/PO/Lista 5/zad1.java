af97831b1da892faba50b9237512dded52952a68
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-03-25 15:06:12 +0100

    PO 5

---
+++ Sem2/PO/Lista 5/zad1.java
@@ -0,0 +1,26 @@
+class OrderedList<T>{
+
+
+    public void add_element(T elem){
+
+    }
+
+    public T get_first(){
+        
+    }
+
+    public String to_string(){
+        
+    }
+
+    public interface Comparable <T> {
+    int compareTo(T o);
+    }
+};
+
+class Program{
+    public static void main(String[] args){
+        System.out.println("HW!\n");
+        
+    }
+};

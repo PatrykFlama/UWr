a0012113ad82774c227a95cedb5c5fb5341af1b7
PatrykFlama <patrykflama@interia.pl>
2023-03-27 11:19:58 +0200

    MP

--- Sem2/MP/Lista 5/zad.rkt
+++ Sem2/MP/Lista 5/zad.rkt
@@ -41,8 +41,8 @@
     (local [
         (define (_perms _inlist elements)   ; [original list for permutation] [elements to pop]
             (cond
-                [(empty? elements) '()]
-                [(empty? (rest _inlist)) (list _inlist)]
+                [(empty? elements) '()] ; empty elements queue
+                [(empty? (rest _inlist)) (list _inlist)]    ; one element left
                 [else (append
                         (map ; add deleted element to all subsets from list without that element 
                             (lambda (x) (cons (first elements) x))
@@ -52,7 +52,61 @@
         (list '())
         (_perms inlist inlist))))
   
-          
+; --- zad5 ---
+(define-type (Tree 'a)
+    (leaf)
+    (node [l : (Tree 'a)] [elem : 'a] [r : (Tree 'a)]))
+
+(define example-tree
+    (node (node (leaf) 1 (leaf))
+          2
+          (node (leaf)
+                3
+                (node (leaf) 4 (leaf)))))
+
+#|
+    f_node      acc res_left node_val res_right
+    f_leaf      acc
+    f_acc_left  acc node_val
+    f_acc_right acc node_val
+    acc/res - from root to node
+    tree
+|#
+(define (process-tree f_node f_leaf f_acc_left f_acc_right acc tree)
+    (if (leaf? tree)
+        (f_leaf acc)
+        (f_node 
+            acc
+            (process-tree f_node f_leaf f_acc_left f_acc_right
+                (f_acc_left acc (node-elem tree))
+                (node-l tree))
+            (node-elem tree)
+            (process-tree f_node f_leaf f_acc_left f_acc_right
+                (f_acc_right acc (node-elem tree))
+                (node-r tree)))))
+
+(define (bst? tree)
+    (process-tree   ; f_node f_leaf f_acc_left f_acc_right acc tree
+        (lambda (acc res_left elem res_right)
+            (and res_left res_right
+                (< (fst acc) elem) (> (snd acc) elem)))
+        (lambda (acc) #t)
+        (lambda (acc elem)
+            (pair (fst acc) elem))
+        (lambda (acc elem)
+            (pair elem (snd acc)))
+        (pair -inf.0 +inf.0) tree))
+
+(define (sum-paths tree)
+    (process-tree   ; f_node f_leaf f_acc_left f_acc_right acc tree
+        (lambda (acc res_left elem res_right)
+            (node res_left (+ acc elem) res_right))
+        (lambda (acc) (leaf))
+        (lambda (acc elem)
+            (+ acc elem))
+        (lambda (acc elem)
+            (+ acc elem))
+        0 tree))
 
 
 
@@ -71,4 +125,8 @@
 
 
 
-              
+
+
+
+
+

65db8d3898f9dab79f435c735440761a32f3cb93
PatrykFlama <patrykflama@interia.pl>
2023-03-27 10:16:51 +0200

    MP 5

---
+++ Sem2/MP/Lista 5/README.md
@@ -0,0 +1,53 @@
+# Lista 5
+| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |
+|---|---|---|---|---|---|---|---|---|
+| X |   |   |   |   |   |   |   |   |
+
+## Zadanie 2
+```
+(define (apply f x) (f x))
+(('a -> 'b) 'a -> 'b)
+```
+Apply aplikuje element x do funkcji f, więc funkcja ta musi przyjmować ten sam typ co x, a zwracać może cokolwiek
+```
+(define (compose f g) (lambda (x) (f (g x))))
+(('a -> 'b) ('c -> 'a) -> ('c -> 'b))
+```
+Compose jest złożeniem funkcji f oraz g, więc f musi przyjmować ten sam typ jaki zwraca g, pozostałe typy są dowolne
+```
+(define (flip f) (lambda (x y) (f y x)))
+(('a 'b -> 'c) -> ('b 'a -> 'c))
+```
+Flip odwraca kolejność argumentów przyjmowanych przez funkcję, więc i kolejność typów
+```
+(define (curry f) (lambda (x) (lambda (y) (f x y))))
+(('a 'b -> 'c) -> ('a -> ('b -> 'c)))
+```
+Curry rozbija funkcję 2 argumentową na złożenie 2 funkcji 1 argumentowych, które muszą przyjmować te same typy co oryginalne funkcje
+
+## Zadanie 3 
+<!-- TODO -->
+```
+( curry compose )
+(('_a -> '_b) -> (('_c -> '_a) -> ('_c -> '_b)))
+```
+Otrzymujemy 'rozbicie' złożenia funkcji, czyli funkcję która przyjmuje 
+```
+(( curry compose ) ( curry compose ))
+(('_a -> ('_b -> '_c)) -> ('_a -> (('_d -> '_b) -> ('_d -> '_c))))
+```
+
+```
+(( curry compose ) ( curry apply ))
+(('_a -> ('_b -> '_c)) -> ('_a -> ('_b -> '_c)))
+```
+
+```
+(( curry apply ) ( curry compose ))
+(('_a -> '_b) -> (('_c -> '_a) -> ('_c -> '_b)))
+```
+
+```
+( compose curry flip )
+(('_a '_b -> '_c) -> ('_b -> ('_a -> '_c)))
+```
--- Sem2/MP/Lista 5/zad.rkt
+++ Sem2/MP/Lista 5/zad.rkt
@@ -11,12 +11,24 @@
 (define (f4 [b : ('a -> 'b)] [c : ('a -> 'c)])
   (lambda (a) (pair (b a) (c a))))
 
-(define (f5 aoab a)
-  (if (some? (aoab a))
-      (cons (snd (some-v (aoab a)))
-            (f5 aoab (fst (some-v (aoab a)))))
-      empty))
+(define (f5 a_ab a)
+  (if (some? (a_ab a))
+      (cons (snd (some-v (a_ab a)))
+            (f5 a_ab (fst (some-v (a_ab a)))))
+      '()))
 
+; --- zad2 ---
+(define (apply f x) (f x))
+(define (compose f g) (lambda (x) (f (g x))))
+(define (flip f) (lambda (x y) (f y x)))
+(define (curry f) (lambda (x) (lambda (y) (f x y))))
+
+; --- zad3 ---
+; ( curry compose )
+; (( curry compose ) ( curry compose ))
+; (( curry compose ) ( curry apply ))
+; (( curry apply ) ( curry compose ))
+; ( compose curry flip )
 
 ; --- zad4 ---
 (define (remove x list)
@@ -25,17 +37,20 @@
     [(equal? x (first list)) (rest list)]
     [else (cons (first list) (remove x (rest list)))]))
 
-(define (_perms _inlist elements)
-  (cond
-    [(empty? elements) '(666)]
-    [(empty? _inlist) '(777)]
-    [else (append
-           (_perms _inlist (rest elements))
-           (_perms (remove (first elements) _inlist) elements))]))
-
-
-(define (perms list)
-  (_perms list list))
+(define (perms inlist)
+    (local [
+        (define (_perms _inlist elements)   ; [original list for permutation] [elements to pop]
+            (cond
+                [(empty? elements) '()]
+                [(empty? (rest _inlist)) (list _inlist)]
+                [else (append
+                        (map ; add deleted element to all subsets from list without that element 
+                            (lambda (x) (cons (first elements) x))
+                            (perms (remove (first elements) _inlist)))
+                        (_perms _inlist (rest elements)))]))]   ; iteration over all elements
+    (if (empty? inlist)
+        (list '())
+        (_perms inlist inlist))))
   
           
 

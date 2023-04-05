705aa196eaa824b92d68b362e30533a8fa0f95a5
PatrykFlama <patrykflama@interia.pl>
2023-04-05 22:06:24 +0200

    CPP done

---
+++ Sem2/CPP/zad5/class.cpp
@@ -0,0 +1,135 @@
+#include "class.hpp"
+
+Color::Color(color_value r = 0, color_value g = 0, color_value b = 0){
+    if(out_of_range(r) || out_of_range(g) || out_of_range(b))
+        throw invalid_argument("Color value out of range");
+    
+    _r = r;
+    _g = g;
+    _b = b;
+}
+
+color_value Color::get_r() const { return _r; }
+color_value Color::get_g() const { return _g; }
+color_value Color::get_b() const { return _b; }
+
+void Color::set_r(color_value r){ 
+    if(out_of_range(r)) throw invalid_argument("Color value out of range");
+    _r = r; 
+}
+void Color::set_g(color_value g){ 
+    if(out_of_range(g)) throw invalid_argument("Color value out of range");
+    _g = g; 
+}
+void Color::set_b(color_value b){ 
+    if(out_of_range(b)) throw invalid_argument("Color value out of range");
+    _b = b; 
+}
+
+void Color::lighten(color_value diff){
+    _r = min(color_value(255), _r+diff);
+    _g = min(color_value(255), _g+diff);
+    _b = min(color_value(255), _b+diff);
+}
+
+void Color::darken(color_value diff){
+    _r = max(color_value(0),   _r-diff);
+    _g = max(color_value(0),   _g-diff);
+    _b = max(color_value(0),   _b-diff);
+}
+
+color_value Color::combine(color_value a, color_value b) {
+    return (a+b)/2;
+}
+
+ostream &operator<< (ostream &out, const Color &c){
+    return out << c.get_r() << ' ' << c.get_g() << ' ' << c.get_b();
+}
+
+TransparentColor::TransparentColor(color_value r = 0, color_value g = 0, color_value b = 0, color_value alpha = 255) : Color(r, g, b){
+    if(out_of_range(alpha)) throw invalid_argument("Alpha channel value out of range");
+    _alpha = alpha;
+}
+
+color_value TransparentColor::get_transparency() const { return _alpha; }
+void TransparentColor::set_transparency(color_value alpha){
+    if(out_of_range(alpha)) throw invalid_argument("Alpha channel value out of range");
+    _alpha = alpha;
+}
+
+ostream &operator<< (ostream &out, const TransparentColor &c){
+    return out << c.get_r() << ' ' << c.get_g() << ' ' << c.get_b() << ' ' << c.get_transparency();
+}
+
+bool NamedColor::check_name(string &name) const {
+    for(auto i : name)
+        if(i > 'z' || i < 'a') return true;
+    return false;
+}
+
+NamedColor::NamedColor(color_value r = 0, color_value g = 0, color_value b = 0, string name = "") : Color(r, g, b){
+    if(check_name(name)) throw invalid_argument("Invalid name (should consist only of small letters)");
+    _name = name;
+}
+
+string NamedColor::get_name() const { return _name; }
+void NamedColor::set_name(string name){
+    if(check_name(name)) throw invalid_argument("Invalid name (should consist only of small letters)");
+    _name = name;
+}
+
+ostream &operator<< (ostream &out, const NamedColor &c){
+    return out << c.get_r() << ' ' << c.get_g() << ' ' << c.get_b() << ' ' << c.get_name();
+}
+
+TransNamColor::TransNamColor(color_value r = 0, color_value g = 0, color_value b = 0,
+                color_value alpha = 255, string name = "") : Color(r, g, b){
+    if(out_of_range(alpha)) throw invalid_argument("Alpha channel value out of range");
+    _alpha = alpha;
+    if(check_name(name)) throw invalid_argument("Invalid name (should consist only of small letters)");
+    _name = name;
+}
+
+ostream &operator<< (ostream &out, const TransNamColor &c){
+    return out << c.get_r() << ' ' << c.get_g() << ' ' << c.get_b() << ' ' << c.get_transparency() << ' ' << c.get_name();
+}
+
+Pixel::Pixel(int x, int y){
+    if(check_pixels(x, y)) throw invalid_argument("Pixels are out of the screen borders");
+    _x = x;
+    _y = y;
+}
+
+int Pixel::get_x() const { return _x; }
+int Pixel::get_y() const { return _y; }
+
+float Pixel::calc_dist(int x, int y) const {
+    return abs(sqrt((_x-x)*(_x-x) + (_y-y)*(_y-y)));
+}
+
+ostream &operator<< (ostream &out, const Pixel &c){
+    return out << c.get_x() << ' ' << c.get_y();
+}
+
+ColorPixel::ColorPixel(int x, int y, color_value r = 0, color_value g = 0, color_value b = 0, color_value alpha = 255)
+    : Pixel(x, y), Color(r, g, b){
+    if(out_of_range(alpha)) throw invalid_argument("Alpha channel value out of range");
+    _alpha = alpha;
+}
+
+void ColorPixel::transform(int dx, int dy){
+    _x += dx;
+    _y += dy;
+    if(check_pixels(_x, _y)) throw invalid_argument("Pixels are out of the screen borders");
+}
+
+ostream &operator<< (ostream &out, const ColorPixel &c){
+    return out << c.get_x() << ' ' << c.get_y() << ' ' << c.get_r() << ' ' << c.get_g() << ' ' << c.get_b() << ' ' << c.get_transparency();
+}
+
+int pixel_distance(const Pixel &p, const Pixel &q){
+    return (int)(p.calc_dist(q.get_x(), q.get_y()));
+}
+int pixel_distance(const Pixel *p, const Pixel *q){
+    return (int)(p->calc_dist(q->get_x(), q->get_y()));
+}
---
+++ Sem2/CPP/zad5/class.hpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// typedef float color_value;
+#define color_value float
+
+
+class Color{
+protected:
+    color_value _r, _g, _b;
+
+    inline bool out_of_range(color_value col) const {
+        return (col < 0 || col > 255);
+    }
+
+public:
+    Color(color_value r, color_value g, color_value b);
+
+    color_value get_r() const;
+    color_value get_g() const;
+    color_value get_b() const;
+
+    void set_r(color_value r);
+    void set_g(color_value g);
+    void set_b(color_value b);
+
+    void lighten(color_value diff);
+
+    void darken(color_value diff);
+
+    static color_value combine(color_value a, color_value b);
+
+    friend ostream &operator<< (ostream &out, const Color &c);
+};
+
+class TransparentColor : public virtual Color{
+protected:
+    color_value _alpha;
+
+public:
+    TransparentColor(color_value r, color_value g, color_value b, color_value alpha);
+
+    color_value get_transparency() const;
+    void set_transparency(color_value alpha);
+
+    friend ostream &operator<< (ostream &out, const TransparentColor &c);
+};
+
+class NamedColor : public virtual Color{
+protected:
+    string _name;
+
+    bool check_name(string &name) const;
+
+public:
+    NamedColor(color_value r, color_value g, color_value b, string name);
+
+    string get_name() const;
+    void set_name(string name);
+
+    friend ostream &operator<< (ostream &out, const NamedColor &c);
+};
+
+//* https://isocpp.org/wiki/faq/multiple-inheritance#mi-diamond
+class TransNamColor : public TransparentColor, public NamedColor{
+public:
+    TransNamColor(color_value r, color_value g, color_value b,
+                  color_value alpha, string name);
+
+    friend ostream &operator<< (ostream &out, const TransNamColor &c);
+};
+
+class Pixel{
+protected:
+    const static int size_x = 1980, size_y = 1080;      // which defaults to static at namespace level
+    int _x, _y;
+
+    inline bool check_pixels(int x, int y) const {
+        return (x < 0 || size_x <= x || y < 0 || size_y <= y);
+    }
+
+public:
+    Pixel(int x, int y);
+
+    int get_x() const;
+    int get_y() const;
+
+    float calc_dist(int x, int y) const;
+
+    inline int calc_dist_left_up()    const { return calc_dist(0, 0); }
+    inline int calc_dist_right_up()   const { return calc_dist(size_x, 0); }
+    inline int calc_dist_left_down()  const { return calc_dist(0, size_y); }
+    inline int calc_dist_right_down() const { return calc_dist(size_x, size_y); }
+
+    friend ostream &operator<< (ostream &out, const Pixel &c);
+};
+
+class ColorPixel : public Pixel, public TransparentColor{
+public:
+    ColorPixel(int x, int y, color_value r, color_value g, color_value b, color_value alpha);
+    
+    void transform(int dx, int dy);
+
+    friend ostream &operator<< (ostream &out, const ColorPixel &c);
+};
+
+int pixel_distance(const Pixel &p, const Pixel &q);
+int pixel_distance(const Pixel *p, const Pixel *q);

99d6ac43b6e9ba7ea59ca7bac773154d6924e76c
PatrykFlama <patrykflama@interia.pl>
2023-04-06 18:50:45 +0200

    MP prac projection done

--- Sem2/MP/Pracownia 1/main.rkt
+++ Sem2/MP/Pracownia 1/main.rkt
@@ -74,14 +74,96 @@ table:
         (table
             (table-schema tab) 
             (cons row (table-rows tab)))
-        (error "table-insert: incorrect row type or length!")))
+        (error 'table-insert "incorrect row type or length!")))
 
 ; Projection of table
 (define (table-project cols tab)
-    )
+    (define (get_column_number column)      ; number of column in the table schema
+        (define (_gcn columns cnt)
+            (cond
+                [(empty? columns)
+                    (error 'table-project "column does not exist!")]
+                [(equal? column (column-info-name (first columns)))
+                    cnt]
+                [else 
+                    (_gcn (rest columns) (+ cnt 1))]))
+        (_gcn (table-schema tab) 0))
 
+    (define (get_cols_nums columns)
+        (if (empty? columns)
+            '()
+            (cons 
+                (get_column_number (first columns))
+                (get_cols_nums (rest columns)))))
+
+    (define (get_row row col_nums)      ; creates row from given list of columns (in order)
+        (if (empty? col_nums)
+            '()
+            (cons
+                (list-ref row (first col_nums))
+                (get_row row (rest col_nums)))))
+    
+    (define (get_rows rows col_nums)
+        (if (empty? rows)
+            '()
+            (cons
+                (get_row (first rows) col_nums)
+                (get_rows (rest rows) col_nums))))
+
+    (define (get_column_info col_nums)
+        (if (empty? col_nums)
+            '()
+            (cons
+                (list-ref (table-schema tab) (first col_nums))
+                (get_column_info (rest col_nums)))))
+
+    (if (empty? cols)
+        (empty-table)
+        (let ((column_numbers (get_cols_nums cols)))
+            (table
+                (get_column_info column_numbers)
+                (get_rows (table-rows tab) column_numbers)))))
 
 #|
+    (define (_get_cell n row)
+        (cond
+            [(null? row) (error 'table-project "_get_cell: column number exceeds row size!")] ;shouldn't happen
+            [(= n 0) (car row)]
+            [else (_get_cell (- n 1) (cdr row))]))
+
+    (define (_get_column n rows)
+        (if (null? rows)
+            '()
+            (cons 
+                (_get_cell n (car rows))
+                (_get_column n (cdr rows)))))
+
+    (define (_combine_columns col1 col2)
+        (if (or (null? col1) (null? col2))
+            '()
+            (cons
+                (cons (car col1) (car col2))
+                (_combine_columns (cdr col1) (cdr col2)))))
+
+    (define (_get_column_schem n tab-schem)
+        (cond
+            [(null? tab-schem) (error 'table-project "_get_col_schem n > len(tab-schem)!")]
+            [(= n 0) (car tab-schem)]
+            [else (_get_column_schem (- n 1) (cdr tab-schem))]))
+
+    (if (null? cols)
+        (table '() '())
+        (let ((table_n_found (get_column_number (car cols)))
+              (table_rest    (table-project (cdr cols) tab)))
+             (table
+                (cons (_get_column_schem table_n_found) (table-schema table_rest))
+                (_combine_columns 
+                    (_get_column 
+                        table_n_found
+                        (table-rows tab))
+                    (table-rows table_rest))))))
+
+
 ; Sorting the table
 (define (table-sort cols tab)
   ;; TODO uzupełnij

4d599c6b0bdda329517d9e28b2651653d8ed6c38
PatrykFlama <patrykflama@interia.pl>
2023-04-17 11:24:55 +0200

    ugh, so typed plait needs types consistency?

--- Sem2/MP/Lista 7/zad_plait.rkt
+++ Sem2/MP/Lista 7/zad_plait.rkt
@@ -7,6 +7,8 @@
     (3-node (l : (2-3-tree 'a)) (elem-l : 'a) (mid : (2-3-tree 'a)) (elem-r : 'a) (r : (2-3-tree 'a))))
     ; TODO e-r < e-l
 
+; TODO example tree (so learn about 2-3 trees)
+
 (define (2-3-tree? t)
     (local(
         (define (check_order minn maxx t)
@@ -23,15 +25,18 @@
                         (check_order minn e-r l)
                         (check_order e-r e-l mid)
                         (check_order e-l maxx r))]))
+
+        (define false -inf.0)
+        (define (t? n) (not (= n false)))
         (define (check_height t)    ; height has to be equal in all subtrees
             (type-case (2-3-tree 'a) t
                 [(leaf) 0]
                 [(2-node l elem r)
                     (let [(h_l (check_height l)) (h_r (check_height r))]
-                    (if (and h_l h_r (= h_l h_r)) (+ h_l 1) #f))]
+                    (if (and (t? h_l) (t? h_r) (= h_l h_r)) (+ h_l 1) false))]
                 [(3-node l e-l mid e-r r)
                     (let [(h_l (check_height l) (h_m (check_height mid)) (h_r (check_height r)))]
-                    (if (and h_l h_m h_r (= h_l h_m h_r)) (+ h_l 1) #f))])))
+                    (if (and (t? h_l) (t? h_m) (t? h_r) (= h_l h_m h_r)) (+ h_l 1) false))])))
     (and 
         (check_order -inf.0 +inf.0 t)
         (check_height t))))

7077328dcd3ba0443f74a0ca242e32d67ec617d7
PatrykFlama <patrykflama@interia.pl>
2023-04-17 09:49:11 +0200

    CPP wip

--- Sem2/CPP/zad6/main.cpp
+++ Sem2/CPP/zad6/main.cpp
@@ -13,36 +13,38 @@ public:
 
     Expression(){}
 
-    Expression(Expression&& other) = default;
-    Expression& operator=(Expression&& other) = default;
-    Expression(const Expression& other) = delete;
-    Expression& operator=(Expression& other) = delete;
+    // Expression(Expression&& other) = default;
+    // Expression& operator=(Expression&& other) = default;
+    // Expression(const Expression& other) = delete;
+    // Expression& operator=(Expression& other) = delete;
 
     type evaluate(){
+        cout <<"EVAL\n";
         return 0;
     }
 
     string toString(){
+        cout << "TS\n";
         return "";
     }
 
-    static void setVariable(string name, type val){
+    void setVariable(string name, type val){
         variables[name] = val;
     }
 
-    static type getVariable(string name){
+    type getVariable(string name){
         auto ptr = variables.find(name);
         if(ptr == variables.end()) throw runtime_error("Variable " + name + " has no assigned value!");
         return ptr->second;
     }
 
-    static void deleteVariable(string name){
+    void deleteVariable(string name){
         variables.erase(name);
     }
 };
 
 /* #region //* operands */
-class Number final : Expression {
+class Number final : public Expression {
     type val;
 public:
     Number(type _val = 0) : val(_val) {}
@@ -56,7 +58,7 @@ public:
     }
 };
 
-class Constant final : Expression {
+class Constant final : public Expression {
     string val;
 public:
     Constant(string _val = "pi") : val(_val) {}
@@ -73,7 +75,7 @@ public:
     }
 };
 
-class Variable final : Expression{
+class Variable final : public Expression{
     string name;
 public:
     Variable(string _name) : name(_name) {}
@@ -92,47 +94,61 @@ public:
 /* #region //* operators 1arg */
 class Operator1arg : public Expression {
 protected:
-    static Expression *e;
+    Expression *e;
 public:
-    Operator1arg(Expression* _e) { e = _e; }
+    Operator1arg(Expression* _e) : e(_e) {}
     ~Operator1arg(){ delete e; }
 
-    static void setVariable(string name, type val){
+    void setVariable(string name, type val){
         e->setVariable(name, val);
     }
-    static type getVariable(string name){
+    type getVariable(string name){
         return e->getVariable(name);
     }
-    static void deleteVariable(string name){
+    void deleteVariable(string name){
         e->deleteVariable(name);
     }
 };
 
-class Sin final : Operator1arg {
+class Sin final : public Operator1arg {
+public:
+    using Operator1arg::Operator1arg;
     type evaluate(){ return sin(e->evaluate()); }
     string toString(){ return "sin(" + e->toString() + ")"; }
 };
-class Cos final : Operator1arg {
+class Cos final : public Operator1arg {
+public:
+    using Operator1arg::Operator1arg;
     type evaluate(){ return cos(e->evaluate()); }
     string toString(){ return "cos(" + e->toString() + ")"; }
 };
-class Exp final : Operator1arg {
+class Exp final : public Operator1arg {
+public:
+    using Operator1arg::Operator1arg;
     type evaluate(){ return exp(e->evaluate()); }
     string toString(){ return "exp(" + e->toString() + ")"; }
 };
-class Ln final : Operator1arg {
+class Ln final : public Operator1arg {
+public:
+    using Operator1arg::Operator1arg;
     type evaluate(){ return log(e->evaluate()); }
     string toString(){ return "ln(" + e->toString() + ")"; }
 };
-class Abs final : Operator1arg {
+class Abs final : public Operator1arg {
+public:
+    using Operator1arg::Operator1arg;
     type evaluate(){ return abs(e->evaluate()); }
     string toString(){ return "|" + e->toString() + "|"; }
 };
-class Opposite final : Operator1arg {
+class Opposite final : public Operator1arg {
+public:
+    using Operator1arg::Operator1arg;
     type evaluate(){ return -(e->evaluate()); }
     string toString(){ return "opposite(" + e->toString() + ")"; }
 };
-class Inverse final : Operator1arg {
+class Inverse final : public Operator1arg {
+public:
+    using Operator1arg::Operator1arg;
     type evaluate(){ return 1/(e->evaluate()); }
     string toString(){ return "inverse(" + e->toString() + ")"; }
 };
@@ -142,41 +158,44 @@ class Inverse final : Operator1arg {
 /* #region //* operators 2arg */
 class Operator2arg : public Expression{
 protected:
-    static Expression *L, *R;
+    Expression *L, *R;
 public:
-    Operator2arg(Expression* _L, Expression* _R) {
-        L = _L;
-        R = _R;
-    }
+    Operator2arg(Expression* _L, Expression* _R) : L(_L), R(_R) {}
     ~Operator2arg(){
         delete L;
         delete R;
     }
 
-    static void setVariable(string name, type val){
+    void setVariable(string name, type val){
         L->setVariable(name, val);
         R->setVariable(name, val);
     }
-    static type getVariable(string name){
+    type getVariable(string name){
         return L->getVariable(name);
     }
-    static void deleteVariable(string name){
+    void deleteVariable(string name){
         L->deleteVariable(name);
         R->deleteVariable(name);
     }
 };
 
-class Add final : Operator2arg {
+class Add final : public Operator2arg {
+public:
+    using Operator2arg::Operator2arg;
     int operation_priority = 0;
     type evaluate(){ return L->evaluate() + R->evaluate(); }
     string toString(){ return L->toString() + " + " + R->toString(); }
 };
-class Sub final : Operator2arg {
+class Sub final : public Operator2arg {
+public:
+    using Operator2arg::Operator2arg;
     int operation_priority = 0;
     type evaluate(){ return L->evaluate() - R->evaluate(); }
     string toString(){ return L->toString() + " - " + R->toString(); }
 };
-class Mult final : Operator2arg {
+class Mult final : public Operator2arg {
+public:
+    using Operator2arg::Operator2arg;
     int operation_priority = 2;
     type evaluate(){ return L->evaluate() * R->evaluate(); }
     string toString(){
@@ -189,7 +208,9 @@ class Mult final : Operator2arg {
         return tempL + " * " + tempR;
     }
 };
-class Div final : Operator2arg {
+class Div final : public Operator2arg {
+public:
+    using Operator2arg::Operator2arg;
     int operation_priority = 2;
     type evaluate(){ return L->evaluate() / R->evaluate(); }
     string toString(){
@@ -202,13 +223,17 @@ class Div final : Operator2arg {
         return tempL + " / " + tempR;
     }
 };
-class Log final : Operator2arg {
+class Log final : public Operator2arg {
+public:
+    using Operator2arg::Operator2arg;
     type evaluate(){ return log(R->evaluate()) / log(L->evaluate()); }
     string toString(){
         return "log(" + L->toString() + ", " + R->toString() + ")";
     }
 };
-class Mod final : Operator2arg {
+class Mod final : public Operator2arg {
+public:
+    using Operator2arg::Operator2arg;
     type evaluate(){ return fmod(R->evaluate(), L->evaluate()); }
     string toString(){
         string tempL = L->toString();
@@ -220,7 +245,9 @@ class Mod final : Operator2arg {
         return tempL + " % " + tempR;
     }
 };
-class Pow final : Operator2arg {
+class Pow final : public Operator2arg {
+public:
+    using Operator2arg::Operator2arg;
     type evaluate(){ return pow(R->evaluate(), L->evaluate()); }
     string toString(){
         return "pow(" + L->toString() + ", " + R->toString() + ")";
@@ -230,5 +257,6 @@ class Pow final : Operator2arg {
 
 
 int main(){
-    Expression *e = new Operator2arg(new Expression(), new Expression());
+    Sub *e = new Sub(new Number(1), new Number(2));
+    cout << e->toString() << ' ' << e->evaluate() << '\n';
 }

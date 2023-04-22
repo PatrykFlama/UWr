483b9257b4a9c610bcd67714090556924c76e814
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-04-22 11:11:39 +0200

    MP deque+delist

--- Sem2/MP/Lista 8/mdeque.rkt
+++ Sem2/MP/Lista 8/mdeque.rkt
@@ -2,15 +2,15 @@
 
 ;TODO provide
 
-; ========DLIST========
-(struct del
-    (prev val next) #:mutable)
+; ========Double Ended List========
+(struct del     ;TODO is that proper name?
+    (prev val next) #:mutable #:transparent)
 
-(define (del-empty? xs) (and
+(define (del-empty? xs) (and    ; TODO probably useless 
     (null? (del-prev xs))
     (null? (del-next xs))))
 
-(define (del-last? xs) (null? (del-next xs)))
+(define (del-last? xs) (null? (del-next xs)))   ;; TODO probably useless
 
 ; =======DEQUE========
 (struct mdeque
@@ -22,46 +22,52 @@
     (null? (mdeque-front dq)))
 (define (nonempty-mdeque? dq) (and
     (mdeque? dq)
-    (not (empty-mdeque dq))))
+    (not (mdeque-empty? dq))))
 
 (define (empty-mdeque) (mdeque null null))
 
-(define (display-mdeque dq) (mdeque-front dq))
+(define (display-mdeque dq) 
+    (displayln (mdeque-front example-mdeque))
+    (displayln (mdeque-back example-mdeque)))
 
 
 (define (mdeque-push-front dq x)
-    (define p (mcons x (mdeque-front dq)))
+    (define p (del null x (mdeque-front dq)))
     (if (mdeque-empty? dq)
         (set-mdeque-back! dq p)
-        (void))
+        (set-del-prev! (mdeque-front dq) p))
     (set-mdeque-front! dq p))
     
 (define (mdeque-push-back dq x)
-    (define p (mcons x null))
+    (define p (del (mdeque-back dq) x null))
     (if (mdeque-empty? dq)
         (set-mdeque-front! dq p)
-        (set-mcdr! (mdeque-back dq) p))
+        (set-del-next! (mdeque-back dq) p))
     (set-mdeque-back! dq p))
 
 (define/contract (mdeque-pop-front dq)
     (-> nonempty-mdeque? any/c)
     (define p (mdeque-front dq))
-    (set-mdeque-front! dq (mcdr p))
-    (if (null? (mcdr p))
+    (set-mdeque-front! dq (del-next p))
+    (if (null? (del-next p))
         (begin
             (set-mdeque-back! dq null)
-            (mcar p))
-        (mcar p)))
+            (del-val p))
+        (begin
+            (set-del-prev! (mdeque-front dq) null)
+            (del-val p))))
 
 (define/contract (mdeque-pop-back dq)
     (-> nonempty-mdeque? any/c)
     (define p (mdeque-back dq))
-    (set-mdeque-front! dq (mcdr p)) ; TODO set last pointer to null
-    (if (null? (mcdr p))
+    (set-mdeque-back! dq (del-prev p))
+    (if (null? (del-prev p))
         (begin
             (set-mdeque-back! dq null)
-            (mcar p))
-        (mcar p)))
+            (del-val p))
+        (begin
+            (set-del-next! (mdeque-back dq) null)
+            (del-val p))))
 
 (define example-mdeque (empty-mdeque))
 (mdeque-push-front example-mdeque 3)
@@ -69,3 +75,10 @@
 (mdeque-push-front example-mdeque 1)
 (mdeque-push-back  example-mdeque 4)
 (mdeque-push-back  example-mdeque 5)
+
+(mdeque-front example-mdeque)
+(mdeque-back example-mdeque)
+(mdeque-pop-front example-mdeque)
+(mdeque-pop-back example-mdeque)
+(mdeque-front example-mdeque)
+(mdeque-back example-mdeque)

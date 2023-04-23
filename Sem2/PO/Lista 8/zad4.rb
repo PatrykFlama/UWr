8944c75abb7a07dbba4891515f3356a9d7591f07
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-04-23 18:23:13 +0200

    PO zad4

---
+++ Sem2/PO/Lista 8/zad4.rb
@@ -0,0 +1,58 @@
+class Jawna
+    def initialize(slowo)
+        @slowo = slowo
+    end
+
+    def to_s
+        return @slowo
+    end
+
+    def zaszyfruj(klucz)
+        slowo = ""
+        @slowo.split('').each { |c|
+            slowo += 
+                if klucz[c] == nil
+                    c
+                else
+                    klucz[c]
+                end
+        }
+        return Zaszyfrowana.new(slowo);
+    end  
+end
+
+class Zaszyfrowana
+    def initialize(slowo)
+        @slowo = slowo
+    end
+
+    def to_s
+        return @slowo
+    end
+
+    def odszyfruj(klucz)
+        slowo = ""
+        klucz = klucz.invert()
+        @slowo.split('').each { |c|
+            slowo += 
+                if klucz[c] == nil
+                    c
+                else
+                    klucz[c]
+                end
+        }
+        return Jawna.new(slowo) 
+    end
+end
+
+
+przykladowe_slowo = Jawna.new("slowo");
+prz_klucz = {'s' => 'a',
+             'l' => 'b',
+             'o' => 'c',
+             'w' => 'd'}
+zaszyfrowane = przykladowe_slowo.zaszyfruj(prz_klucz)
+
+print przykladowe_slowo, " => "
+print zaszyfrowane, " => "
+print zaszyfrowane.odszyfruj(prz_klucz), "\n" 

e1d7ff5a4590db9e1583bd57488dffe2880db4bf
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-04-26 08:05:49 +0200

    AI zad3

--- Sem2/AI/Lista 3/sudoku.py
+++ Sem2/AI/Lista 3/sudoku.py
@@ -1,22 +1,22 @@
 import sys
 
 
-def V(i,j):
-    return 'V%d_%d' % (i,j)
-    
+def V(i, j):
+    return 'V%d_%d' % (i, j)
+
 def domains(Vs):
-    return [q + ' in 1..9' for q in Vs ]
-    
+    return [q + ' in 1..9' for q in Vs]
+
 def all_different(Qs):
     return 'all_distinct([' + ', '.join(Qs) + '])'
-    
+
 def get_column(j):
-    return [V(i,j) for i in range(9)]
+    return [V(i, j) for i in range(9)]
 
 def get_raw(i):
-    return [V(i,j) for j in range(9)]     
+    return [V(i, j) for j in range(9)]
 
-def horizontal():   
+def horizontal():
     return [all_different(get_raw(i)) for i in range(9)]
 
 def vertical():
@@ -26,53 +26,58 @@ def get_square(i, j):
     inside = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
     return [V(i + k[0], j + k[1]) for k in inside]
 
-def in_square():
+def in_cell():
     squares = [(0, 0), (0, 3), (0, 6), (3, 3), (3, 6), (6, 6), (3, 0), (6, 0)]
     return [all_different(get_square(s[0], s[1])) for s in squares]
 
 
 def print_constraints(Cs, indent, d):
     position = indent
-    print(indent * ' ', end='')
+    print((indent - 1) * ' ', end=' ', file=out_file)
     for c in Cs:
-        print (c + ',', end=' ')
+        print(c + ',', end=' ', file=out_file)
         position += len(c)
         if position > d:
             position = indent
-            print ()
-            print (indent * ' ', end='')
+            print(file=out_file)
+            print(indent * ' ', end=' ', file=out_file)
 
-      
+
 def sudoku(assigments):
-    variables = [V(i,j) for i in range(9) for j in range(9)]
-    
-    print (':- use_module(library(clpfd)).')
-    print ('solve([' + ', '.join(variables) + ']) :- ')
-    
-    
-    cs = domains(variables) + vertical() + horizontal() + in_square()
-    for i,j,val in assigments:
-        cs.append( '%s #= %d' % (V(i,j), val))
-    
+    variables = [V(i, j) for i in range(9) for j in range(9)]
+
+    print(':- use_module(library(clpfd)).', file=out_file)
+    print('solve([' + ', '.join(variables) + ']) :- ', file=out_file)
+
+    cs = domains(variables) + vertical() + horizontal() + in_cell()
+    for i, j, val in assigments:
+        cs.append('%s #= %d' % (V(i, j), val))
+
     print_constraints(cs, 4, 70),
-    print ()
-    print ('    labeling([ff], [' +  ', '.join(variables) + ']).')
-    print ()
-    print (':- solve(X), write(X), nl.')       
+    print(file=out_file)
+    print('    labeling([ff], [' + ', '.join(variables) + ']).', file=out_file)
+    print(file=out_file)
+    print(':- solve(X), write(X), nl.', file=out_file)
+
 
 if __name__ == "__main__":
+    out_file = open("zad_output.txt", "w", encoding='utf8')
+    input_file = open("zad_input.txt", encoding='utf8').readlines()
+
     raw = 0
     triples = []
-    
-    for x in sys.stdin:
+
+    for x in input_file:
         x = x.strip()
         if len(x) == 9:
             for i in range(9):
                 if x[i] != '.':
-                    triples.append( (raw,i,int(x[i]))) 
+                    triples.append((raw, i, int(x[i])))
             raw += 1
-        else: break
     sudoku(triples)
+
+    out_file.close()
+
     
 """
 89.356.1.

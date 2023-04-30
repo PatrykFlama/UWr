a17bc4e0ff676e6a32092c51b2175978a7ffbf97
PatrykFlama <patrykflama@interia.pl>
2023-04-30 21:44:51 +0200

    cpp wip

--- Sem2/CPP/zad8/rational.cpp
+++ Sem2/CPP/zad8/rational.cpp
@@ -1,27 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+namespace exceptions {
+    class division_by_zero : public exception {
+        const char* what() const throw() {
+            return "Division by 0!";
+        }
+    };
+    class out_of_range : public exception {
+        const char* what() const throw() {
+            return "Out of range!";
+        }
+    };
+}
+
 namespace calculations {
     class Rational {
         int numerator, denominator;
 
-        int nwd(int a, int b) {
+        int nwd(int a, int b) noexcept {
             if (b == 0) return a;
             return nwd(b, a % b);
         }
 
-        void reduce() {
+        void reduce() noexcept {
             int n = nwd(numerator, denominator);
             numerator /= n;
             denominator /= n;
         }
 
     public:
-        Rational() {
+        Rational() noexcept {
             numerator = 0;
             denominator = 1;
         }
-        Rational(int numerator = 0, int denominator = 1) {
+        Rational(int numerator = 0, int denominator = 1) noexcept {
             this->numerator = numerator;
             this->denominator = denominator;
             repair();
@@ -35,7 +48,7 @@ namespace calculations {
             }
         }
 
-        string toString() {
+         string toString() noexcept {
             return to_string(numerator) + "/" + to_string(denominator);
         }
 
@@ -61,6 +74,7 @@ namespace calculations {
         Rational operator+(Rational other) {
             int n = numerator * other.denominator + other.numerator * denominator;
             int d = denominator * other.denominator;
+            // TODO: check if n/d is in range
             return Rational(n, d);
         }
         Rational operator-(Rational other) {
@@ -78,11 +92,11 @@ namespace calculations {
             int d = denominator * other.numerator;
             return Rational(n, d);
         }
-        Rational operator-(){
+        Rational operator-() {
             return Rational(-numerator, denominator);
         }
-        Rational operator!(){
-            if(numerator < 0) throw exception();
+        Rational operator!() {
+            if(numerator < 0) throw exceptions::division_by_zero();
             return Rational(denominator, numerator);
         }
 
@@ -91,6 +105,11 @@ namespace calculations {
         }
         explicit operator int() {
             return numerator/denominator;
+        }
+
+        friend ostream& operator<<(ostream& out, Rational r) {
+            out << r.toString();
+            return out;
         }
         /* #endregion */
     };

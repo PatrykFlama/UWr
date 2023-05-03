d4d48d1bbf851d89e0e1c610a818cbaa41136892
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-05-03 11:10:51 +0200

    POPOPOPOPOPOPO

--- Sem2/PO/Lista 9/zad.rb
+++ Sem2/PO/Lista 9/zad.rb
@@ -1,5 +1,5 @@
 # Patryk Flama
-# Zadanie 1
+# Zadanie 1 i 3
 # Kompilacja: ruby zad.rb
 
 class Function
@@ -42,12 +42,52 @@ class Function
     def deriv(x, h = 0.0001)
         return (value(x + h) - value(x)) / h
     end
+
+    def plot(x_from, y_from, x_to, y_to, e = 0.5)
+        (y_from..y_to).reverse_each do |y|
+            print " "
+            
+            (x_from..x_to).each do |x|
+            begin
+                if (value(x) - y).abs < e
+                    print "#"
+                elsif x == 0 and y == 0
+                    print "+"
+                elsif x == 0
+                    print "|"
+                elsif y == 0
+                    print "-"
+                else
+                    print " "
+                end
+            rescue Exception
+                if x == 0 and y == 0
+                    print "+"
+                elsif x == 0
+                    print "|"
+                elsif y == 0
+                    print "-"
+                else
+                    print " "
+                end
+                end
+            end
+
+            puts " "
+        end
+    end
 end
 
 sq = Function.new proc {|x| x*x}
-
 puts sq.value(2)
 print sq.zero(0, 2, 0.0001), "\n"
 puts sq.field(0, 2)
 puts sq.deriv(2)
-print sq.pixels(0, 0, 10, 10, 1), "\n"
+print sq.plot(-4, -1, 4, 10), "\n"
+
+sin = Function.new proc {|x| Math.sin(x)}
+puts sin.value(2)
+print sin.zero(0, 2, 0.0001), "\n"
+puts sin.field(0, 2)
+puts sin.deriv(2)
+print sin.plot(-10, -2, 10, 2), "\n"

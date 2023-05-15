51b2b00c4b05bda1b765487eceab73c7782ead02
PatrykFlama <patrykflama@interia.pl>
2023-05-15 13:07:47 +0200

    'proper' solution

--- Sem2/PARO/zad2/exercises/example_leak_raii.cpp
+++ Sem2/PARO/zad2/exercises/example_leak_raii.cpp
@@ -6,30 +6,29 @@
 #include <memory> // std::unique_ptr
 
 using Event = int;
+using UniqueResource = std::unique_ptr<RESOURCE, decltype(&free_resource)>;
 
-std::unique_ptr<Event, decltype(&free_resource)> create_event(int event_id) noexcept { 
-    return std::unique_ptr<Event, decltype(&free_resource)>(new Event(event_id), &free_resource);
+std::unique_ptr<Event> create_event(int event_id) noexcept { 
+    return std::make_unique<Event>(event_id);
 }
 
-void process_init(RESOURCE* resource) noexcept
+void process_init(UniqueResource resource) noexcept
 try {
     if (resource) {
-        use_resource(resource);
-        free_resource(resource);
+        use_resource(resource.get());
     }
 } catch (std::runtime_error const& ex) {
     std::cout << ex.what() << std::endl;
 }
 
-void process_event(std::unique_ptr<Event, decltype(&free_resource)> event,
-                   RESOURCE* temp_resource,
+void process_event(std::unique_ptr<Event> event,
+                   UniqueResource temp_resource,
                    RESOURCE* proc_resource) noexcept
 try {
     std::cout << "Processing Event(" << *event << ")" << std::endl;
 
     if (temp_resource) {
-        use_resource(temp_resource);
-        free_resource(temp_resource);
+        use_resource(temp_resource.get());
     }
 
     if (proc_resource) {
@@ -43,21 +42,17 @@ try {
 
 int main(int ac, char* av[])
 {
-    auto init_resource = allocate_resource();
-    process_init(init_resource);
+    auto init_resource = UniqueResource(allocate_resource(), &free_resource);
+    process_init(std::move(init_resource));
 
-    auto proc_resource = allocate_resource();
+    auto proc_resource = UniqueResource(allocate_resource(), &free_resource);
 
     std::vector<int> simulation = {
         12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 132, 144};
     for (auto id : simulation) {
         auto event = create_event(id);
-        auto temp_resource = allocate_resource();
-        process_event(move(event), temp_resource, proc_resource);
-    }
-
-    if (proc_resource) {
-        free_resource(proc_resource);
+        auto temp_resource = UniqueResource(allocate_resource(), &free_resource);
+        process_event(move(event), std::move(temp_resource), proc_resource.get());
     }
 
     return 0;

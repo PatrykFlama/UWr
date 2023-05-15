eeb0fa26f64d4e88a52fbcc0e9b6e92b82a424ef
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-05-15 16:19:52 +0200

    MP 3 done

--- Sem2/MP/Lista 10/kody_z_wykladu/bool.rkt
+++ Sem2/MP/Lista 10/kody_z_wykladu/bool.rkt
@@ -21,36 +21,40 @@
   (ifE [b : Exp]
        [l : Exp]
        [r : Exp])
-  (condE [cs : (Listof (Exp * Exp))]))
+  ; (condE [cs : (Listof (Exp * Exp))])
+  (trueE)
+  (falseE))
 
 ;; parse ----------------------------------------
 
 (define (parse [s : S-Exp]) : Exp
   (cond
+    [(s-exp-match? `#t s) (trueE)]
+    [(s-exp-match? `#f s) (falseE)]
     [(s-exp-match? `NUMBER s)
      (numE (s-exp->number s))]
     [(s-exp-match? `{if ANY ANY ANY} s)
      (ifE (parse (second (s-exp->list s)))
           (parse (third (s-exp->list s)))
           (parse (fourth (s-exp->list s))))]
-    [(s-exp-match? `{cond ANY ...} s)
-     (condE (parse-cond (rest (s-exp->list s))))]
+    ; [(s-exp-match? `{cond ANY ...} s)
+    ;  (condE (parse-cond (rest (s-exp->list s))))]
     [(s-exp-match? `{SYMBOL ANY ANY} s)
      (opE (parse-op (s-exp->symbol (first (s-exp->list s))))
           (parse (second (s-exp->list s)))
           (parse (third (s-exp->list s))))]
     [else (error 'parse "invalid input")]))
 
-(define (parse-cond [ss : (Listof S-Exp)]) : (Listof (Exp * Exp))
-  (type-case (Listof S-Exp) ss
-    [empty
-     empty]
-    [(cons s ss)
-     (if (s-exp-match? `{ANY ANY} s)
-         (cons (pair (parse (first (s-exp->list s)))
-                     (parse (second (s-exp->list s))))
-               (parse-cond ss))
-         (error 'parse "invalid input: cond"))]))
+; (define (parse-cond [ss : (Listof S-Exp)]) : (Listof (Exp * Exp))
+;   (type-case (Listof S-Exp) ss
+;     [empty
+;      empty]
+;     [(cons s ss)
+;      (if (s-exp-match? `{ANY ANY} s)
+;          (cons (pair (parse (first (s-exp->list s)))
+;                      (parse (second (s-exp->list s))))
+;                (parse-cond ss))
+;          (error 'parse "invalid input: cond"))]))
 
 (define (parse-op [op : Symbol]) : Op
   (cond
@@ -82,13 +86,13 @@
   (test/exn (parse `{+ 1})
             "invalid input")
   (test/exn (parse `{^ 1 2})
-            "unknown operator")
-  (test (parse `{cond {{= 0 1} {* 3 4}}
-                      {{= 1 1} 8}})
-        (condE (list (pair (opE (eql) (numE 0) (numE 1))
-                           (opE (mul) (numE 3) (numE 4)))
-                     (pair (opE (eql) (numE 1) (numE 1))
-                           (numE 8))))))
+            "unknown operator"))
+  ; (test (parse `{cond {{= 0 1} {* 3 4}}
+  ;                     {{= 1 1} 8}})
+  ;       (condE (list (pair (opE (eql) (numE 0) (numE 1))
+  ;                          (opE (mul) (numE 3) (numE 4)))
+  ;                    (pair (opE (eql) (numE 1) (numE 1))
+  ;                          (numE 8))))))
   
 ;; eval --------------------------------------
 
@@ -132,24 +136,26 @@
 (define (eval [e : Exp]) : Value
   (type-case Exp e
     [(numE n) (numV n)]
+    [(trueE) (boolV #t)]
+    [(falseE) (boolV #f)]
     [(opE o l r) ((op->proc o) (eval l) (eval r))]
     [(ifE b l r)
      (type-case Value (eval b)
        [(boolV v)
         (if v (eval l) (eval r))]
        [else
-        (error 'eval "type error")])]
-    [(condE cs)
-     (eval (cond->if cs))]))
+        (error 'eval "type error")])]))
+    ; [(condE cs)
+    ;  (eval (cond->if cs))]))
 
-(define (cond->if [cs : (Listof (Exp * Exp))]) : Exp
-  (type-case (Listof (Exp * Exp)) cs
-    [empty
-     (numE 42)]
-    [(cons c cs)
-     (ifE (fst c)
-          (snd c )
-          (cond->if cs))]))
+; (define (cond->if [cs : (Listof (Exp * Exp))]) : Exp
+;   (type-case (Listof (Exp * Exp)) cs
+;     [empty
+;      (numE 42)]
+;     [(cons c cs)
+;      (ifE (fst c)
+;           (snd c )
+;           (cond->if cs))]))
 
 (define (run [e : S-Exp]) : Value
   (eval (parse e)))
@@ -166,10 +172,10 @@
   (test (run `{= 0 1})
         (boolV #f))
   (test (run `{if {= 0 1} {* 3 4} 8})
-        (numV 8))
-  (test (run `{cond {{= 0 1} {* 3 4}}
-                    {{= 1 1} 8}})
         (numV 8)))
+  ; (test (run `{cond {{= 0 1} {* 3 4}}
+  ;                   {{= 1 1} 8}})
+        ; (numV 8)))
 
 ;; printer ———————————————————————————————————-
 
@@ -183,3 +189,54 @@
 
 (define (main [e : S-Exp]) : Void
   (print-value (eval (parse e))))
+
+;; abstract machine ---------------------------
+
+(define-type Stack
+  (emptyS)
+  (rightS [op : Op] [exp : Exp] [s : Stack])
+  (leftS [op : Op] [val : Value] [s : Stack])
+  (ifS [exp1 : Exp] [exp2 : Exp] [s : Stack]))
+
+(define (evalAM [e : Exp] [s : Stack]) : Value
+  (type-case Exp e
+    [(numE n)
+     (continueAM s (numV n))]
+    [(trueE)
+     (continueAM s (boolV #t))]
+    [(falseE)
+     (continueAM s (boolV #f))]
+    [(opE o e1 e2)
+     (evalAM e1 (rightS o e2 s))]
+    [(ifE condition e1 e2) 
+     (evalAM condition (ifS e1 e2 s))]))
+
+(define (continueAM [s : Stack] [v : Value]) : Value
+  (type-case Stack s
+    [(emptyS)
+     v]
+    [(rightS op e s)
+     (evalAM e (leftS op v s))]
+    [(leftS op u s)
+     (continueAM s ((op->proc op) v u))]
+    [(ifS e1 e2 s)
+     (type-case Value v
+       [(boolV condition) (if condition
+            (evalAM e1 s)
+            (evalAM e2 s))]
+       [(numV _)
+        (error 'continueAM "if expression type error")])]))
+  
+(define (runAM [e : S-Exp]) : Value
+  (evalAM (parse e) (emptyS)))
+
+(module+ test
+  (test (run `2)
+        (runAM `2))
+  (test (run `{+ 2 1})
+        (runAM `{+ 2 1}))
+  (test (run `{* 2 1})
+        (runAM `{* 2 1}))
+  (test (run `{+ {* 2 3} {+ 5 8}})
+        (runAM `{+ {* 2 3} {+ 5 8}})))
+

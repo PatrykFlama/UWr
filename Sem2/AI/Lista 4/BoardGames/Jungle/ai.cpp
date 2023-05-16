afa853885467f4ef5c72232b6b80aa84eb062481
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-05-16 13:59:20 +0200

    AI setup

--- Sem2/AI/Lista 4/BoardGames/Jungle/ai.cpp
+++ Sem2/AI/Lista 4/BoardGames/Jungle/ai.cpp
@@ -23,10 +23,10 @@ Rat cant beat from ~water to .land
 only Tiger and Lion can jump over ~water (not instanteously, and not over the Rat)
 ----------------
 animal abilities groups:
-1. Rat
-2. Cat, Dog, Wolf, Jaguar
-3. Tiger, Lion
-4. Elephant
+1. Rat                          - can go into water, can eat Elephant
+2. Cat, Dog, Wolf, Jaguar       - can do nothing
+3. Tiger, Lion                  - can jump over water
+4. Elephant                     - can do nothing
 */
 
 const char board[9][7] = {
@@ -40,8 +40,51 @@ const char board[9][7] = {
     {'.', '.', '.', '#', '.', '.', '.'},
     {'.', '.', '#', '*', '#', '.', '.'}};
 
+const char starting_positions[9][7] = {
+    {'L', '.', '.', '.', '.', '.', 'T'},
+    {'.', 'D', '.', '.', '.', 'C', '.'},
+    {'R', '.', 'J', '.', 'W', '.', 'E'},
+    {'.', '.', '.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.', '.', '.'},
+    {'e', '.', 'w', '.', 'j', '.', 'r'},
+    {'.', 'c', '.', '.', '.', 'd', '.'},
+    {'t', '.', '.', '.', '.', '.', 'l'}};
+
 
 class Jungle {
+    const int DIRS[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // R D L U
+public:
     vector<pair<int, int>> player_pieces;   // positions of R C D W J T L E; -1 -1 if eaten (rat can eat elephant)
-    vector<pair<int, int>> enemy_pieces;
+    vector<pair<int, int>> opponent_pieces;
+    vector<pair<int, int>> player_force_jump_direction; // direction of jump for tiger and lion; 0 0 if no force jump
+    vector<pair<int, int>> opponent_force_jump_direction;
+
+    Jungle(){
+        reset();
+    }
+
+    void reset(){
+        player_pieces = {{0, 2}, {6, 1}, {1, 1}, {5, 2}, {2, 2}, {7, 0}, {0, 0}, {7, 2}};
+        opponent_pieces.reserve(8);
+        for(auto [x, y] : player_pieces)
+            opponent_pieces.push_back({7 - x, 9 - y});
+        player_force_jump_direction = {{0, 0}, {0, 0}};
+        opponent_force_jump_direction = {{0, 0}, {0, 0}};
+    }
+
+    void move_player_piece(int dx, int dy, int piece_index){
+        auto [x, y] = player_pieces[piece_index];
+        player_pieces[piece_index] = {x + dx, y + dy};
+    }
+    void move_opponent_piece(int dx, int dy, int piece_index){
+        auto [x, y] = opponent_pieces[piece_index];
+        opponent_pieces[piece_index] = {x + dx, y + dy};
+    }
+
+    
 };
+
+int main() {
+
+}

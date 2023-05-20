ef392911346a46eb50eb20e387aa93d40379518d
PatrykFlama <patrykflama@interia.pl>
2023-05-20 17:20:18 +0200

    better random added

--- Sem2/AI/Lista 4/BoardGames/Jungle/ai.cpp
+++ Sem2/AI/Lista 4/BoardGames/Jungle/ai.cpp
@@ -303,6 +303,7 @@ public:
         for(auto [piece, dir] : legal_moves){
             Jungle temp = state->gen_next_state(piece, dir);
             int score = random(&temp, N);
+            score += reinforced_random(&temp, N);
             if(score > max_score){
                 max_score = score;
                 best_move = {piece, dir};
@@ -340,6 +341,53 @@ public:
 
         // cerr << "Games played: " << games_played << " res: " << rate(total_res, games_played) << '\n';
         return total_res;
+    }
+
+    int reinforced_random(Jungle* state, int max_games){
+        vector<pair<int, pair<int, int>>> starting_legal_moves = state->get_legal_moves();
+        if(state->terminal(starting_legal_moves)) return state->result(main_player)*max_games;
+
+        Jungle here = *state;
+        int total_res = 0, games_played = 0;
+        vector<pair<int, pair<int, int>>> legal_moves;
+        while(games_played < max_games){
+            legal_moves = here.get_legal_moves();
+            if(here.terminal(legal_moves)) {
+                total_res += here.result(main_player);
+                ++games_played;
+                here = *state;      // copy 
+                legal_moves = starting_legal_moves;
+            }
+
+            int best_piece;
+            pair<int, int> best_dir;
+            int max_score = INT_MIN;
+            for(auto [piece, dir] : legal_moves){
+                Jungle temp = here.gen_next_state(piece, dir);
+                int score = heuristics(&temp);
+                if(score > max_score){
+                    max_score = score;
+                    best_piece = piece;
+                    best_dir = dir;
+                }
+            }
+            here.execute_move(best_piece, best_dir);
+        }
+
+        // cerr << "Games played: " << games_played << " res: " << rate(total_res, games_played) << '\n';
+        return total_res;
+    }
+
+    int heuristics(Jungle *state){
+        int res = 0;
+        for(auto [x, y] : state->pieces[main_player]){
+            res += dist(x, y, (main_player ? upper_den : lower_den));
+        }
+        return res;
+    }
+
+    inline int dist(int x, int y, pair<int, int> den){
+        return sqrt((x+den.first)*(x+den.first) + (y+den.second)*(y+den.second));
     }
 };
 
@@ -433,9 +481,9 @@ int main() {
     bool display = false;
     srand(time(NULL));
     Jungle game;
-    Random ai1;
-    zad3AI ai2;
-    int MAX_GAMES = 100;
+    Random ai0;
+    zad3AI ai1;
+    int MAX_GAMES = 10;
     int win_counter[2] = {0, 0};
 
     while(MAX_GAMES--){
@@ -447,7 +495,7 @@ int main() {
         while(not game.game_won()){
             turns++;
             if(turn){
-                auto move = ai1.gen_next_move(&game);
+                auto move = ai0.gen_next_move(&game);
                 if(display) cout << "ai1_cap move: " << AnimalNames[move.first] << ' ' << move.second.first << ' ' << move.second.second << ' '; 
                 auto [myxs, myys] = game.pieces[game.player][move.first];
                 if(display) cout << "(" << myxs << ' ' << myys << ' ' << myxs+move.second.first << ' ' << myys+move.second.second << ")\n";
@@ -455,7 +503,7 @@ int main() {
                 game.execute_move(move);
                 turn = 1-turn;
             } else{
-                auto move = ai2.gen_next_move(&game);
+                auto move = ai1.gen_next_move(&game);
                 if(display) cout << "ai2_low move: " << AnimalNames[move.first] << ' ' << move.second.first << ' ' << move.second.second << ' '; 
                 auto [myxs, myys] = game.pieces[game.player][move.first];
                 if(display) cout << "(" << myxs << ' ' << myys << ' ' << myxs+move.second.first << ' ' << myys+move.second.second << ")\n";
@@ -471,8 +519,8 @@ int main() {
         game.reset();
     }
 
-    cout << "ai1 wins: " << win_counter[0] << '\n';
-    cout << "ai2 wins: " << win_counter[1] << '\n';
+    cout << "ai0 wins: " << win_counter[0] << '\n';
+    cout << "ai1 wins: " << win_counter[1] << '\n';
 }
 
 

367a5b1b57d813d2727d82ea0ec9b9805b34551a
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-05-30 16:22:35 +0200

    wip

--- Sem2/AI/CGspring23/wooden2.cpp
+++ Sem2/AI/CGspring23/wooden2.cpp
@@ -3,24 +3,26 @@ using namespace std;
 
 
 class Cell {
-    public:
-        int index, cellType, resources, myAnts, oppAnts;
-        vector<int> neighbors;
+public:
+    int index, cellType, resources, myAnts, oppAnts;
+    vector<int> neighbors;
 
-        Cell(int index, int cellType, int resources, vector<int> neighbors, int myAnts = 0, int oppAnts = 0) {
-            index = index;
-            cellType = cellType;
-            resources = resources;
-            neighbors = neighbors;
-            myAnts = myAnts;
-            oppAnts = oppAnts;
-        }
+    Cell(int index, int cellType, int resources, vector<int> neighbors, int myAnts = 0, int oppAnts = 0) {
+        index = index;
+        cellType = cellType;
+        resources = resources;
+        neighbors = neighbors;
+        myAnts = myAnts;
+        oppAnts = oppAnts;
+    }
 };
 
 int main() {
     vector<Cell> cells;
     vector<int> myBases, oppBases;
     int numberOfCells; // amount of hexagonal cells in this map
+
+    // ------------------ INIT INPUT ------------------
     cin >> numberOfCells; cin.ignore();
     for (int i = 0; i < numberOfCells; i++) {
         int type; // 0 for empty, 1 for eggs, 2 for crystal
@@ -32,7 +34,7 @@ int main() {
         int neigh4;
         int neigh5;
         cin >> type >> initialResources >> neigh0 >> neigh1 >> neigh2 >> neigh3 >> neigh4 >> neigh5; cin.ignore();
-        Cell cell(i, type, initialResources, {neigh0, neigh1, neigh2, neigh3, neigh4, neigh5}, 0, 0);
+        Cell cell(i, type, (type == 0 ? 0 : initialResources), {neigh0, neigh1, neigh2, neigh3, neigh4, neigh5}, 0, 0);
         cells.push_back(cell);
     }
     
@@ -50,8 +52,8 @@ int main() {
     }
 
     // game loop
-    while (1) {
-        string actions = "WAIT";
+    while (true) {
+        // ------------------ TURN INPUT ------------------
         for (int i = 0; i < numberOfCells; i++) {
             int resources; // the current amount of eggs/crystals on this cell
             int myAnts; // the amount of your ants on this cell
@@ -63,14 +65,11 @@ int main() {
             cells[i].oppAnts = oppAnts;
         }
 
-        //TODO: choose actions to perform and add them into actions
-        // To debug: cerr << "Debug messages..." << endl;
-        // WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
-        if (actions.length() == 0){
-            cout << "WAIT" << endl;
-        } else {
-            actions += ';';
-            cout << actions << endl;
+        // ------------------ TURN LOGIC && OUTPUT ------------------
+        for(auto i : cells) {
+            cout << "LINE " << myBases[0] << ' ' << i.index << ' ' << i.resources << ';';
         }
+        cout << endl;
     }
 }
+

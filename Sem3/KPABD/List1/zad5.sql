5aac249cf848039a814fde2eba009fd50f792150
PatrykFlama <patrykflama@interia.pl>
2023-10-14 19:01:19 +0200

    sequeling

--- Sem3/KPABD/List1/zad5.sql
+++ Sem3/KPABD/List1/zad5.sql
@@ -1,4 +1,4 @@
-SELECT h.SalesOrderID, h.SalesOrderNumber, h.PurchaseOrderNumber, SUM(LineTotal) AS TotalAfterDiscount, SUM(LineTotal) + SUM(UnitPrice*UnitPriceDiscount*OrderQty) AS TotalBeforeDiscount, SUM(OrderQty) AS TotalQuantity
+SELECT TOP 1 h.SalesOrderID, h.SalesOrderNumber, h.PurchaseOrderNumber, SUM(LineTotal) AS TotalAfterDiscount, SUM(LineTotal) + SUM(UnitPrice*UnitPriceDiscount*OrderQty) AS TotalBeforeDiscount, SUM(OrderQty) AS TotalQuantity
 FROM [SalesLT].[SalesOrderDetail] d LEFT JOIN [SalesLT].[SalesOrderHeader] h ON d.SalesOrderID = h.SalesOrderID
 GROUP BY h.SalesOrderID, h.SalesOrderNumber, h.PurchaseOrderNumber
 ORDER BY SUM(UnitPrice*UnitPriceDiscount*OrderQty) DESC

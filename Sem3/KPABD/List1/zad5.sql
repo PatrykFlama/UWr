d3a0c84242c3cb02a6c6fb7f80335488e8bcf1a6
PatrykFlama <patrykflama@interia.pl>
2023-10-14 14:32:10 +0200

    nice nice

--- Sem3/KPABD/List1/zad5.sql
+++ Sem3/KPABD/List1/zad5.sql
@@ -1,3 +1,6 @@
-SELECT TOP 1 h.SalesOrderID, SalesOrderNumber, PurchaseOrderNumber, LineTotal, (LineTotal-UnitPrice*UnitPriceDiscount) AfterDiscount, OrderQty
+SELECT h.SalesOrderID, h.SalesOrderNumber, h.PurchaseOrderNumber, SUM(LineTotal) AS TotalAfterDiscount, SUM(LineTotal) + SUM(UnitPrice*UnitPriceDiscount*OrderQty) AS TotalBeforeDiscount, SUM(OrderQty) AS TotalQuantity
 FROM [SalesLT].[SalesOrderDetail] d LEFT JOIN [SalesLT].[SalesOrderHeader] h ON d.SalesOrderID = h.SalesOrderID
-ORDER BY UnitPrice*UnitPriceDiscount DESC
+GROUP BY h.SalesOrderID, h.SalesOrderNumber, h.PurchaseOrderNumber
+ORDER BY SUM(UnitPrice*UnitPriceDiscount*OrderQty) DESC
+
+-- according to documentation, linetotal is total price with discount included
--- Sem3/KPABD/List1/zad6.sql
+++ Sem3/KPABD/List1/zad6.sql
@@ -1,4 +1,5 @@
-SELECT c.FirstName, c.LastName, SUM(UnitPrice*UnitPriceDiscount)
+SELECT c.FirstName, c.LastName, SUM(UnitPrice*UnitPriceDiscount*OrderQty) MoneySaved
 FROM ([SalesLT].[SalesOrderDetail] d LEFT JOIN [SalesLT].[SalesOrderHeader] h ON d.SalesOrderID = h.SalesOrderID) 
 	LEFT JOIN [SalesLT].[Customer] c ON h.CustomerID = c.CustomerID
 GROUP BY c.CustomerID, c.FirstName, c.LastName
+ORDER BY SUM(UnitPrice*UnitPriceDiscount*OrderQty) DESC

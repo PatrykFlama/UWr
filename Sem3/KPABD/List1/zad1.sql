501195c3203ceb663a455c241b74f48167f25326
PatrykFlama <patrykflama@interia.pl>
2023-10-17 21:16:09 +0200

    v2v4v8

--- Sem3/KPABD/List1/zad1.sql
+++ Sem3/KPABD/List1/zad1.sql
@@ -1,4 +1,8 @@
-SELECT DISTINCT City FROM 
-[SalesLT].[SalesOrderHeader] LEFT JOIN [SalesLT].[Address] ON ShipToAddressID = AddressID
-WHERE ShipDate IS NOT NULL
+SELECT DISTINCT City 
+FROM [SalesLT].[Address]
+WHERE AddressID IN (SELECT ShipToAddressID FROM [SalesLT].[SalesOrderHeader])
+AND AddressID NOT IN (
+    SELECT ShipToAddressID 
+    FROM [SalesLT].[SalesOrderHeader]
+    WHERE ShipDate IS NULL)
 ORDER BY 1

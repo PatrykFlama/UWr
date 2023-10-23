effd898319299531226b2141b30cac756c1caa38
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-10-23 10:23:24 +0200

    arctg != arcctg

--- Sem3/AN/Lista3/zad1.cpp
+++ Sem3/AN/Lista3/zad1.cpp
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 # define M_PI 3.14159265358979323846
-#define float double
+// #define float double
+
+
+float actg(float x){
+    return atan(1./x);
+}
 
 
 float a(float x){
@@ -23,11 +28,11 @@ float b_better(float x){
 
 
 float c(float x){
-    return pow(x, -3) * (M_PI/2. - x - atan(x));
+    return pow(x, -3) * (M_PI/2. - x - actg(x));
 }
 
 float c_better(float x){
-    return pow(x, -3) * (M_PI/2. - 2.*x - pow(x, 3)/3. + pow(x, 5)/5.);
+    return pow(x, 6)/9. - pow(x, 4)/7. + pow(x, 2)/5. -1./3.;
 }
 
 
@@ -36,9 +41,9 @@ int main(){
     cout << "a) " << a(-10000.) << "\n";
     cout << "a_better) " << a_better(-10000.) << "\n\n";
 
-    cout << "b) " << b(4.001) << "\n";
-    cout << "b_better) " << b_better(4.001) << "\n\n";
+    cout << "b) " << b(4.000001) << "\n";
+    cout << "b_better) " << b_better(4.000001) << "\n\n";
 
-    cout << "c) " << c(0.86033358901938) << "\n";
-    cout << "c_better) " << c_better(0.86033358901938) << "\n\n";
+    cout << "c) " << c(0.000000000001) << "\n";
+    cout << "c_better) " << c_better(0.000000000001) << "\n\n";
 }

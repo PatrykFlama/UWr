5e020d2450ca6a8ea80d845f5eb2c785f55d461c
PatrykFlama <patrykflama@interia.pl>
2023-10-23 19:36:06 +0200

    weppo

--- Sem3/WEPPO/Lista3/zad3.js
+++ Sem3/WEPPO/Lista3/zad3.js
@@ -1,17 +1,21 @@
 function createFs(n) { // tworzy tablicę n funkcji
     var fs = []; // i-ta funkcja z tablicy ma zwrócić i
-    for (var i = 0; i < n; i++) {
-        fs[i] =
-            function () {
-                1
-                return i;
-            };
+    
+    var helper = function helper(i) {     // tworzymy funkcję pomocniczą, która dzięki temu że przyjmuje wartość jako argument, zrywa wiązanie z oryginalną zmienną
+        fs[i] = function () {
+            return i;
+        };
     };
+
+    for (var i = 0; i < n; i++) {
+        helper(i);
+    }
+    
     return fs;
 }
-
 var myfs = createFs(10);
 console.log(myfs[0]()); // zerowa funkcja miała zwrócić 0
 console.log(myfs[2]()); // druga miała zwrócić 2
 console.log(myfs[7]());
-// 10 10 10 // ale wszystkie zwracają 10!?
+
+// skorygować można za pomocą let, ponieważ let tworzy zmienną w bloku, natomiast var jest usuwany wtedy, kiedy nie ma już do niego referencji

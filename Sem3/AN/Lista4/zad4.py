d9c6ae204a9e5e0622fb26e05ea69cf7caf00b61
PatrykFlama <patrykflama@interia.pl>
2023-10-29 21:00:58 +0100

    pyplotting

--- Sem3/AN/Lista4/zad4.py
+++ Sem3/AN/Lista4/zad4.py
@@ -1,22 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 # Define the functions
 def g(x):
     return x**4
 
 def h(x):
-    return x + 4
+    if x + 4 > 0:
+        return math.log(x + 4)
+    else:
+        return float('nan')
 
 # Generate x values
-x = np.linspace(-10, 10, 100)
+x = np.linspace(-4, 2, 100)
+plt.ylim(-5, 10)
+
+# Apply math.log() to each element of x
+y = []
+for i in x:
+    y.append(h(i))
 
 # Plot the functions
 plt.plot(x, g(x), label='g(x)')
-plt.plot(x, h(x), label='h(x)')
+plt.plot(x, y,    label='h(x)')
 
 # Add title and legend
-plt.title('Plot of g(x) = x^4 and h(x) = x + 4')
+plt.title('Plot of g(x) = x^4 and h(x) = log(x + 4)')
 plt.legend()
 
 # Show the plot

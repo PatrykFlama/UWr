e9a2f41b6616d3b7501989b325faa74dccb55546
PatrykFlama <patrykflama@interia.pl>
2023-10-29 20:54:13 +0100

    sync

--- Sem3/AN/Lista4/zad7.cpp
+++ Sem3/AN/Lista4/zad7.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define float double
 const float e = 1e-6;
-float a = 2;
+float a;
 
 float f(float x){
-    return x*x*a;
+    return x*x - a;
 }
 
 float F(float x){
@@ -16,12 +17,13 @@ bool test_for_conv(float x){
     int N = 1000;
     float last = std::numeric_limits<float>::infinity();
 
-    while(N-- && abs(f(x) - f(last)) > e && abs(f(x)) > e){
+    // while(N-- && abs(x - last) > e && abs(f(x)) > e){
+    while(N--){
         last = x;
         x = F(x);
     }
 
-    return abs(x - sqrt(a)) < e;
+    return abs(abs(x) - sqrt(a)) < e;
 }
 
 float bins(bool opt = true){
@@ -36,6 +38,9 @@ float bins(bool opt = true){
 
 
 int main(){
+    a = 0.6;
     float limitR = bins(), limitL = bins(false);
     cout << limitR << ' ' << limitL << '\n';
+    // cout << test_for_conv(1/2.+1/8.) << '\n';
+    // cout << test_for_conv(-2) << '\n';
 }

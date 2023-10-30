03f4ff3fc244f41b7758503377794b72392cd862
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-10-30 17:17:26 +0100

    s

--- Sem3/AN/Lista4/zad5.cpp
+++ Sem3/AN/Lista4/zad5.cpp
@@ -8,27 +8,26 @@ float F(float x){
     return x * (2. - x*R);
 }
 
-
 float calc_res(float x){
     const float e = 1e-4;
-    int N = 1e6;
+    int N = 6;
     int i = 0;
 
     while(N-- && abs(1/R - x) > e){
+        cout << x << '\n';
         x = F(x);
         i++;
     }
 
     if(abs(1/R - x) <= e) max_iter = (max_iter == 0) ? i : (max_iter + i) / 2;
-
     return x;
 }
 
-
-
 int main(){
-    float x0 = 0.5;
+    cout << setprecision(15) << fixed;
+    float x0 = R;
     float x = calc_res(x0);
+    cout << "starting from " << x0 << '\n';
     cout << "x = " << x << '\n';
     cout << "max_iter = " << max_iter << '\n';
 }

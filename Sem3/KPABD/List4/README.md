cd29e5b2baa908bb4c5fb5ac96608dbc293ddb36
PatrykFlama <patrykflama@interia.pl>
2023-11-22 16:22:47 +0100

    z1

--- Sem3/KPABD/List4/README.md
+++ Sem3/KPABD/List4/README.md
@@ -3,4 +3,14 @@
 # List 4
 | 1 | 2 | 3 | 4 | 5 | 6 |
 |---|---|---|---|---|---|
-|   |   |   |   |   |   |
+| X |   |   |   |   |   |
+
+## About triggers
+**DML Triggers**: These triggers are fired when a DML (Data Manipulation Language) event occurs (INSERT, UPDATE, or DELETE). They are further classified into two types:  
+
+**After Triggers (or FOR Triggers)**: These triggers fire after the triggering action occurs.   
+* **Instead Of Triggers**: These triggers fire instead of the triggering action. They are primarily used with views.  
+* **DDL Triggers**: These triggers are fired in response to DDL (Data Definition Language) events such as CREATE, ALTER, and DROP.  
+
+**Logon Triggers**: These triggers are fired in response to a LOGON event, which is raised when a user sessions is being established.  
+
---
+++ Sem3/KPABD/List4/zad1.sql
@@ -0,0 +1,36 @@
+DROP TRIGGER IF EXISTS UpdateModifiedDate
+GO
+
+CREATE TRIGGER UpdateModifiedDate
+ON SalesLT.Customer
+AFTER UPDATE
+AS
+BEGIN
+    UPDATE SalesLT.Customer
+    SET ModifiedDate = GETDATE()
+    FROM SalesLT.Customer c
+    INNER JOIN inserted i ON c.CustomerID = i.CustomerID
+END
+GO
+
+-- TEST:
+INSERT INTO SalesLT.Customer (FirstName, LastName, CompanyName, Phone, PasswordHash, PasswordSalt) 
+    VALUES ('John', 'Snow', 'workworkwork', '123456789', 'hash', 'salt');
+
+SELECT FirstName, ModifiedDate FROM SalesLT.Customer
+WHERE LastName = 'Snow' AND Phone = '123456789';
+GO
+
+WAITFOR DELAY '00:00:01';
+GO
+
+UPDATE SalesLT.Customer
+SET FirstName = 'NotJohn'
+WHERE LastName = 'Snow' AND Phone = '123456789';
+
+SELECT FirstName, ModifiedDate FROM SalesLT.Customer
+WHERE LastName = 'Snow' AND Phone = '123456789';
+
+DELETE FROM SalesLT.Customer
+WHERE LastName = 'Snow' AND Phone = '123456789';
+GO

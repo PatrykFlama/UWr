47021fbad22c95dad9364fd94f7789ed751b7f86
PatrykFlama <patrykflama@interia.pl>
2023-11-29 16:54:08 +0100

    i salto

--- Sem3/KPABD/List5/zad2.sql
+++ Sem3/KPABD/List5/zad2.sql
@@ -23,56 +23,55 @@ INSERT INTO Test VALUES(5, 'P5', 1)
 GO
 
 -- Transaction 1 --
-BEGIN TRAN
-UPDATE Test SET Pcs = 12 WHERE ID = 1
-UPDATE Test SET Pcs = 3  WHERE ID = 4
--- czekamy na przyjêcie pieniêdzy od klienta, st¹d delay
-WAITFOR DELAY '00:00:05'
-SELECT * FROM Test -- dane zmieniaja sie, ale rollback je cofa
-ROLLBACK TRANSACTION
-SELECT * FROM Test
+-- BEGIN TRAN
+-- UPDATE Test SET Pcs = 12 WHERE ID = 1
+-- UPDATE Test SET Pcs = 3  WHERE ID = 4
+-- WAITFOR DELAY '00:00:05'
+-- SELECT * FROM Test
+-- ROLLBACK TRANSACTION
+-- SELECT * FROM Test
 
 -- Transaction 2 --
-SELECT * FROM Test
+-- SELECT * FROM Test
 
 
 -------- Nonrepeatable read --------
 DROP TABLE IF EXISTS People 
 GO
 
-CREATE TABLE People (Imie VARCHAR(20), Nazwisko VARCHAR(20), Wiek INT)
-INSERT INTO People VALUES('Tomasz', 'Kowalski', 20)
-INSERT INTO People VALUES('Oskar',  'Nowak',    21)
-INSERT INTO People VALUES('Jan',    'Kowalski', 22)
-INSERT INTO People VALUES('Jan',    'Nowak',    23)
-INSERT INTO People VALUES('Tomasz', 'Nowak',    24)
+CREATE TABLE People (Name VARCHAR(20), Surname VARCHAR(20), Age INT)
+INSERT INTO People VALUES('Name1', 'Surname1', 20)
+INSERT INTO People VALUES('Name2', 'Surname2', 21)
+INSERT INTO People VALUES('Name3', 'Surname3', 22)
+INSERT INTO People VALUES('Name4', 'Surname4', 23)
+INSERT INTO People VALUES('Name5', 'Surname5', 24)
 GO
 
 -- Transaction 1 --
-BEGIN TRAN
-SELECT * FROM People
-WAITFOR DELAY '00:00:05'
-SELECT * FROM People
-ROLLBACK
-GO
+-- BEGIN TRAN
+-- SELECT * FROM People
+-- WAITFOR DELAY '00:00:05'
+-- SELECT * FROM People
+-- ROLLBACK
+-- GO
 
 -- Transaction 2 --
-BEGIN TRAN
-UPDATE People SET Wiek = 50 WHERE Imie = 'Tomasz' OR Imie = 'Oskar'
-COMMIT
-GO
+-- BEGIN TRAN
+-- UPDATE People SET Age = 30 WHERE Name = 'Name1' OR Name = 'Name2'
+-- COMMIT
+-- GO
 
 
 -------- Phantom read --------
 DROP TABLE IF EXISTS Cars
 GO
 
-CREATE TABLE Cars (Model VARCHAR(20), Marka VARCHAR(20))
-INSERT INTO Cars VALUES('Opel',  'Astra')
-INSERT INTO Cars VALUES('Opel',  'Meriva')
-INSERT INTO Cars VALUES('Skoda', 'Fabia')
-INSERT INTO Cars VALUES('Skoda', 'Superb')
-INSERT INTO Cars VALUES('Skoda', 'Felicia')
+CREATE TABLE Cars (Model VARCHAR(20), Brand VARCHAR(20))
+INSERT INTO Cars VALUES('Model1', 'Brand1')
+INSERT INTO Cars VALUES('Model1', 'Brand2')
+INSERT INTO Cars VALUES('Model1', 'Brand3')
+INSERT INTO Cars VALUES('Model2', 'Brand4')
+INSERT INTO Cars VALUES('Model2', 'Brand5')
 GO
 
 -- Transaction 1 --
@@ -84,7 +83,8 @@ ROLLBACK
 
 -- Transaction 2 --
 BEGIN TRAN
-DELETE FROM Cars WHERE Model = 'Opel'
+DELETE FROM Cars WHERE Model = 'Model1'
 COMMIT
 GO
+
 

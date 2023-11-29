237bb3486acd03c12273a4d150cdba6a305e8d44
PatrykFlama <patrykflama@interia.pl>
2023-11-29 16:27:49 +0100

    fyi

--- Sem3/KPABD/List5/zad2.sql
+++ Sem3/KPABD/List5/zad2.sql
@@ -5,12 +5,12 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 -- SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
 
 -- POZIOM IZOLACJI		DIRTY READ		NONREAPEATABLE READ		PHANTOM
--- READ UNCOMMITTED		TAK				TAK						TAK
--- READ COMMITTED		NIE				TAK						TAK
--- REPEATABLE READ		NIE				NIE						TAK
--- SERIALIZABLE			NIE				NIE						NIE
+-- READ UNCOMMITTED		YES				YES						YES
+-- READ COMMITTED		NO 				YES						YES
+-- REPEATABLE READ		NO 				NO 						YES
+-- SERIALIZABLE			NO 				NO 						NO 
 
--- Odczyt brudnych danych --
+-------- Dirty read --------
 DROP TABLE IF EXISTS Test
 GO
 
@@ -22,14 +22,7 @@ INSERT INTO Test VALUES(4, 'P4', 6)
 INSERT INTO Test VALUES(5, 'P5', 1)
 GO
 
-/*
-Sposób dzia³ania: odpalamy w dwóch okienkach SSMS, pierwsz¹ transakcjê wykonujemy normalnie, po czym
-jednoczeœnie w³¹czamy drug¹. Oczekiwanym wynikiem by³oby wyœwietlenie 12 pi³ek i 3 rowerów w drugiej
-transakcji, gdy¿ zosta³a ona wykonana jednoczeœnie z pierwsz¹, jednak przez to, ¿e musimy czekaæ do
-momentu wykonania pierwszej transakcji (i rollbacku), drugie zapytanie wyœwietli niezmienione dane.
-*/
-
--- Transakcja 1 --
+-- Transaction 1 --
 BEGIN TRAN
 UPDATE Test SET Pcs = 12 WHERE ID = 1
 UPDATE Test SET Pcs = 3  WHERE ID = 4
@@ -39,10 +32,11 @@ SELECT * FROM Test -- dane zmieniaja sie, ale rollback je cofa
 ROLLBACK TRANSACTION
 SELECT * FROM Test
 
--- Transakcja 2 --
+-- Transaction 2 --
 SELECT * FROM Test
 
--- Niepowtarzaloœæ odczytów --
+
+-------- Nonrepeatable read --------
 DROP TABLE IF EXISTS People 
 GO
 
@@ -54,13 +48,7 @@ INSERT INTO People VALUES('Jan',    'Nowak',    23)
 INSERT INTO People VALUES('Tomasz', 'Nowak',    24)
 GO
 
-/*
-Sposób dzia³ania jak przy odczycie brudnych danych, tutaj dochodzi do sytuacji, w której dane s¹
-zmieniane w drugiej transakcji, przez co dwa odczyty danych w pierwszej transakcji zwracaj¹ inne
-wyniki.
-*/
-
--- Transakcja 1 --
+-- Transaction 1 --
 BEGIN TRAN
 SELECT * FROM People
 WAITFOR DELAY '00:00:05'
@@ -68,13 +56,14 @@ SELECT * FROM People
 ROLLBACK
 GO
 
--- Transakcja 2 --
+-- Transaction 2 --
 BEGIN TRAN
 UPDATE People SET Wiek = 50 WHERE Imie = 'Tomasz' OR Imie = 'Oskar'
 COMMIT
 GO
 
--- Odczyty fantomów --
+
+-------- Phantom read --------
 DROP TABLE IF EXISTS Cars
 GO
 
@@ -86,19 +75,14 @@ INSERT INTO Cars VALUES('Skoda', 'Superb')
 INSERT INTO Cars VALUES('Skoda', 'Felicia')
 GO
 
-/*
-Sposób dzia³ania jak wy¿ej, jednak przy odczytach zmienia siê liczba odczytywanych danych
-spowodowana dzia³aniem INSERT lub DELETE.
-*/
-
--- Transakcja 1 --
+-- Transaction 1 --
 BEGIN TRAN
 SELECT * FROM Cars
 WAITFOR DELAY '00:00:05'
 SELECT * FROM Cars
 ROLLBACK
 
--- Transakcja 2 --
+-- Transaction 2 --
 BEGIN TRAN
 DELETE FROM Cars WHERE Model = 'Opel'
 COMMIT

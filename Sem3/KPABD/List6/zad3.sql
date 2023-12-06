9d682aee8770581dba2511fe6e10cfcc9172275e
PatrykFlama <patrykflama@interia.pl>
2023-12-06 17:43:19 +0100

    3

--- Sem3/KPABD/List6/zad3.sql
+++ Sem3/KPABD/List6/zad3.sql
@@ -1,13 +1,45 @@
--- BEGIN: Create Clustered Index
-CREATE CLUSTERED INDEX IX_Ksiazka ON Ksiazka (column1, column2);
--- END: Create Clustered Index
+SELECT
+    TableName = t.name, 
+    ClusteredIndexName = i.name,
+    ColumnName = c.Name
+FROM
+    sys.tables t
+INNER JOIN 
+    sys.indexes i ON t.object_id = i.object_id
+INNER JOIN 
+    sys.index_columns ic ON i.index_id = ic.index_id AND i.object_id = ic.object_id
+INNER JOIN 
+    sys.columns c ON ic.column_id = c.column_id AND ic.object_id = c.object_id
+WHERE
+    i.index_id = 1  -- clustered index
+    AND EXISTS (SELECT * 
+                FROM sys.columns c2 
+                WHERE ic.object_id = c2.object_id AND c2.is_identity = 1)
+GO
 
--- BEGIN: Create Non-Clustered Index
-CREATE NONCLUSTERED INDEX IX_Egzemplarz ON Egzemplarz (column3, column4);
--- END: Create Non-Clustered Index
 
--- BEGIN: Create Covering Index
-CREATE NONCLUSTERED INDEX IX_Covering ON Ksiazka (column1, column2)
-INCLUDE (column3, column4);
--- END: Create Covering Index
+DROP INDEX Ksiazka_PK ON Ksiazka;
+CREATE CLUSTERED INDEX ClusteredIX_Ksiazka ON Ksiazka (Ksiazka_ID, Rok_Wydania DESC, Tytul ASC);
 
+SELECT TOP 10 Rok_Wydania, Tytul FROM Ksiazka;
+
+
+DROP INDEX ClusteredIX_Ksiazka ON Ksiazka;
+GO
+
+CREATE NONCLUSTERED INDEX NonClusteredIX_Egzemplarz ON Egzemplarz (Ksiazka_ID, Sygnatura);
+
+SELECT TOP 10 Sygnatura, Ksiazka_ID FROM Egzemplarz;
+
+DROP INDEX NonClusteredIX_Egzemplarz ON Egzemplarz;
+GO
+
+-- Index optimized for join
+CREATE NONCLUSTERED INDEX IX_Egzemplarz_Ksiazka_ID ON Egzemplarz (Ksiazka_ID);
+CREATE NONCLUSTERED INDEX IX_Ksiazka_ID ON Ksiazka (Ksiazka_ID);
+
+SELECT TOP 10 * FROM Ksiazka JOIN Egzemplarz ON Ksiazka.Ksiazka_ID = Egzemplarz.Ksiazka_ID;
+
+DROP INDEX IX_Egzemplarz_Ksiazka_ID ON Egzemplarz;
+DROP INDEX IX_Ksiazka_ID ON Ksiazka;
+GO

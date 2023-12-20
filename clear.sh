65af8876750ecd74b8c87cf4964fa38fc0670b6e
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-12-20 17:40:35 +0100

    stinky pycashe

--- clear.sh
+++ clear.sh
@@ -1,2 +1,2 @@
-find \( -name '*.exe' -or -name '*.dll' -or -name '*.class' -or -name '*.bak' \) -print0 | xargs -0 rm
+find \( -name '*.exe' -or -name '*.dll' -or -name '*.class' -or -name '*.bak' -or -name '__pycache__' \) -print0 | xargs -0 rm -rf
 #  -or -name '*input.txt' -or -name '*output.txt'

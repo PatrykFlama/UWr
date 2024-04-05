8cdb281d69888625f669298fc29b9c17eb501053
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2024-04-05 08:16:37 +0200

    idkd

--- Sem4/dotNET/List5/Zad1.cs
+++ Sem4/dotNET/List5/Zad1.cs
@@ -3,16 +3,27 @@ using BenchmarkDotNet.Running;
 
 namespace Zad1
 {
+    public class Calculate
+    {
+        public int statical(int x, int y)
+        {
+            return x + y;
+        }
+
+        public dynamic dynamical(dynamic x, dynamic y)
+        {
+            return x + y;
+        }
+    }
+    
+    public class Benchmark
+    {
+        public int N;
+        public int x, y;
+    }
+
     public class ComparisonBenchmark
     {
-        [Params(5, 10)]
-        public int x;
-
-        [Params(10, 20)]
-        public int y;
-
-
-
         [Benchmark]
         public int DoWork1_Concrete(int x, int y)
         {
@@ -31,16 +42,6 @@ namespace Zad1
         public static void Main(string[] args)
         {
             var summary = BenchmarkRunner.Run<ComparisonBenchmark>();
-
-            
-
-            //int x = 5;
-            //int y = 10;
-            //dynamic dynamicX = 5;
-            //dynamic dynamicY = 10;
-
-            //var summary1 = BenchmarkRunner.Run<ComparisonBenchmark>(new BenchmarkDotNet.Configs.DefaultConfig().WithArguments(new[] { "--x", x.ToString(), "--y", y.ToString() }));
-            //var summary2 = BenchmarkRunner.Run<ComparisonBenchmark>(new BenchmarkDotNet.Configs.DefaultConfig().WithArguments(new[] { "--dynamicX", dynamicX.ToString(), "--dynamicY", dynamicY.ToString() }));
         }
     }
 }

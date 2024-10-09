9227b0d7ceee959647417b454899b4076ec3d6da
Patryk Flama <51850536+PatrykFlama@users.noreply.github.com>
2024-10-09 14:17:36 +0200

    some more greedy stuff

--- Sem5/AI2/CodingameFallChallenge/main.cpp
+++ Sem5/AI2/CodingameFallChallenge/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define pii pair<int, int>
 #define cerr if(1)cerr
 
+
 /*
 IDEA
 at the beginning of each month we are given all information required to simulate it 
@@ -42,13 +43,67 @@ enum MoveType {
 /* #endregion */
 
 /* #region STRUCTS */
+// ----- BASIC STRUCTS -----
+class Point {
+public:
+    int x;
+    int y;
+
+    Point() {}
+    Point(int x, int y) : x(x), y(y) {}
+
+    double dist(const Point& p) const {
+        return sqrt(pow((p.x - x), 2) + pow((p.y - y), 2));
+    }
+
+    friend ostream& operator<<(ostream& os, const Point& p) {
+        os << p.x << " " << p.y;
+        return os;
+    }
+};
+
+class Segment {
+    const double epsilon = 0.0000001;
+public:
+    Point p1;
+    Point p2;
+
+    Segment() {}
+    Segment(Point p1, Point p2) : p1(p1), p2(p2) {}
+
+    double length() const {
+        return p1.dist(p2);
+    }
+
+    bool is_point_on_segment(const Point& C) const {
+        return abs(p1.dist(C) + p2.dist(C) - p1.dist(p2)) < epsilon;
+    }
+
+    int orientation(const Point& C) const {
+        int prod = (C.y - p1.y) * (p2.x - p1.x) - (p2.y - p1.y) * (C.x - p1.x);
+        return (prod > 0) - (prod < 0); // returns 1, 0, or -1
+    }
+
+    bool intersects_segment(const Segment& s) const {
+        const Point& C = s.p1, D = s.p2;
+        return this->orientation(C) * this->orientation(D) < 0 && s.orientation(p1) * s.orientation(p2) < 0;
+    }
+
+    friend ostream& operator<<(ostream& os, const Segment& s) {
+        os << s.p1 << " " << s.p2;
+        return os;
+    }
+};
+
+// ----- GAME STRUCTS -----
+
 class Building {
 public:
     int id;
-    pii pos;
+    Point pos;
 
     Building() {}
-    Building(int id, pii pos) : id(id), pos(pos) {}
+    Building(int id, Point pos) : id(id), pos(pos) {}
 };
 
 class LandingPad : public Building {
@@ -56,7 +111,7 @@ public:
     vector<pii> astronauts; // {type, amt}  sorted by amt
 
     LandingPad() {}
-    LandingPad(int id, pii pos, vector<pii> astronauts) : Building(id, pos), astronauts(astronauts) {}
+    LandingPad(int id, Point pos, vector<pii> astronauts) : Building(id, pos), astronauts(astronauts) {}
 };
 
 class Destination : public Building {
@@ -64,7 +119,7 @@ public:
     int type;   
 
     Destination() {}
-    Destination(int id, pii pos, int type) : Building(id, pos), type(type) {}
+    Destination(int id, Point pos, int type) : Building(id, pos), type(type) {}
 };
 
 class Pod {
@@ -84,7 +139,8 @@ public:
     int capacity;
 
     Tube() {}
-    Tube(int id, int building1, int building2, int capacity) : id(id), building1(building1), building2(building2), capacity(capacity) {}
+    Tube(int id, int building1, int building2, int capacity=1) : 
+        id(id), building1(building1), building2(building2), capacity(capacity) {}
 };
 
 class Teleporter {
@@ -94,7 +150,8 @@ public:
     int building_to;
 
     Teleporter() {}
-    Teleporter(int id, int building_from, int building_to) : id(id), building_from(building_from), building_to(building_to) {}
+    Teleporter(int id, int building_from, int building_to) : 
+            id(id), building_from(building_from), building_to(building_to) {}
 };
 
 class Move {
@@ -113,94 +170,47 @@ public:
     unordered_map<int, Tube> tubes;
     unordered_map<int, Teleporter> teleporters;
     unordered_map<int, Pod> pods;
-    vector<Destination> buildings;
-    vector<LandingPad> pads;
+    unordered_map<int, Destination> buildings;
+    unordered_map<int, LandingPad> pads;
 
-    unordered_map<int, vector<pii>> graph;  // {fromBuildingID, [{toBuildingID, cost}]}
-    vector<vector<int>> dist;  // dist[from][to] = cost
+    // unordered_map<int, vector<pii>> graph;  // {fromBuildingID, [{toBuildingID, cost}]}
+    // vector<vector<int>> dist;  // dist[from][to] = cost
 
     Solution() {}
     Solution(int resources, vector<Tube> &tubes, vector<Teleporter> &teleporters, vector<Pod> &pods, vector<Destination> &buildings, vector<LandingPad> &pads) {
-        this->resources = resources;
-        this->buildings = buildings;
-        this->pads = pads;
+        recreate_info(resources, tubes, teleporters, pods);
+        add_buildings(buildings, pads);
+    }
 
+    void recreate_info(int resources, vector<Tube> &tubes, vector<Teleporter> &teleporters, vector<Pod> &pods) {
+        this->resources = resources;
+
+        for (Tube& tube : tubes) {
+        }
+        (this->tubes).clear();
         for(Tube& tube : tubes) {
             this->tubes[tube.id] = tube;
         }
+        (this->teleporters).clear();
         for(Teleporter& teleporter : teleporters) {
             this->teleporters[teleporter.id] = teleporter;
         }
+        (this->pods).clear();
         for(Pod& pod : pods) {
             this->pods[pod.id] = pod;
-        }
-
-        for(Tube& tube : tubes) {
-            graph[tube.building1].push_back({tube.building2, 1});
-            graph[tube.building2].push_back({tube.building1, 1});
-        }
-        for(Teleporter& teleporter : teleporters) {
-            graph[teleporter.building_from].push_back({teleporter.building_to, 0});
         }
     }
 
     void add_buildings(vector<Destination> &buildings, vector<LandingPad> &pads) {
         for(Destination& building : buildings) {
-            this->buildings.push_back(building);
+            this->buildings[building.id] = building;
         }
         for(LandingPad& pad : pads) {
-            this->pads.push_back(pad);
+            this->pads[pad.id] = pad;
         }
     }
-
-    void recalc_shortest() {
-        // TODO floyd warshall does not have to be recalculated every time - new buildings can be simply added
-        int n = buildings.size();
-        dist = vector<vector<int>>(n, vector<int>(n, INT_MAX));
-        for(int i = 0; i < n; i++) {
-            dist[i][i] = 0;
-            for(pii& p : graph[i]) {
-                dist[i][p.first] = p.second;
-            }
-        }
-        for(int k = 0; k < n; k++) {
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-                }
-            }
-        }
-    }
-
-    int eval() {    // some heuristic
-        //! floyd warshall instead of dijkstra for easy recalc
-        // from each landing pad check the best route (with time) to needed destination
-        // then simulate the travelsal and calc score
-
-        recalc_shortest();
-
-        int score = 0;
-        for(LandingPad& pad : pads) {
-            for(pii& astronaut : pad.astronauts) {
-                const int dest = astronaut.first;
-                const int amt = astronaut.second;
-
-                int best_time = 1e9;
-                for(Destination& building : buildings) {
-                    if(building.type == dest)
-                        best_time = min(best_time, dist[building.id][dest]);
-                }
-
-                score += best_time * amt;   //! strong heuristic here (can be improved)
-            }
-        }
-
-        return score;
-    }
-
 
     friend ostream& operator<<(ostream& os, const Solution& sol) {
-        os << "Solution: \n";
         os << "Resources: " << sol.resources << '\n';
         os << "Tubes: \n";
         for(auto& tube : sol.tubes) {
@@ -219,16 +229,16 @@ public:
             os << '\n';
         }
         os << "Buildings: \n";
-        for(const Destination& building : sol.buildings) {
-            os << building.id << " " << building.pos.first << " " << building.pos.second << " " << building.type << '\n';
+        for(const auto& [id, building] : sol.buildings) {
+            os << building.id << " " << building.pos.x << " " << building.pos.y << " " << building.type << '\n';
         }
         os << "Landing pads: \n";
-        for(const LandingPad& pad : sol.pads) {
-            os << pad.id << " " << pad.pos.first << " " << pad.pos.second << "\n";
+        for(const auto& [id, pad] : sol.pads) {
+            os << pad.id << " " << pad.pos.x << " " << pad.pos.y << ": ";
             for(const pii& astronaut : pad.astronauts) {
-                os << astronaut.first << " " << astronaut.second << " ";
+                os << astronaut.first << " " << astronaut.second << "";
             }
-            os << '\n';
+            os << "\n";
         }
 
         return os;
@@ -236,10 +246,10 @@ public:
 
 
     vector<Move> greedy_teleporters() {
-        vector<Move> moves;
-        for(LandingPad& pad : pads) {
-            if(resources < COSTS[TELEPORT]) break;
-
+        //TODO prefer buildings with the least number of ways to get to
+        // sort pads by the number of astronauts
+        vector<int> pads_order;
+        for(auto& [id, pad] : pads) {
             // check if pad already has teleporter
             bool has_teleporter = false;
             for(auto& teleporter : teleporters) {
@@ -250,9 +260,21 @@ public:
             }
             if(has_teleporter) continue;
 
+            pads_order.push_back(id);
+        }
+
+        sort(pads_order.begin(), pads_order.end(), [&](int a, int b) {
+            return pads[a].astronauts[0].second > pads[b].astronauts[0].second;
+        });
+
+        vector<Move> moves;
+        for(int id : pads_order) {
+            LandingPad& pad = pads[id];
+            if(resources < COSTS[TELEPORT]) break;
+
             for(pii& astronaut : pad.astronauts) {
                 bool has_building = false;
-                for(Destination& building : buildings) {
+                for(auto& [id, building] : buildings) {
                     if(building.type != astronaut.first) 
                         continue;
 
@@ -270,15 +292,152 @@ public:
                     teleporters[teleporters.size()*10] = {teleporters.size()*10, pad.id, building.id};
                     resources -= COSTS[TELEPORT];
                     has_building = true;
+
+                    //! TODO KINDA STINKY TRICK
+                    astronaut.second = INT_MIN;
+                    sort(pad.astronauts.begin(), pad.astronauts.end(), [](pii& a, pii& b) {
+                        return a.second > b.second;
+                    });
+
                     break;
                 }
                 if(has_building) break;
             }
-
-            // find fitting building that is not occupied
         }
 
         return moves;
+    }
+
+    vector<Move> greedy_tubes() {
+        vector<Move> moves;
+        for(auto& [id, pad] : pads) {
+            // check if pad has > 4 tubes
+            {
+                int occupancy = 0;
+                for(auto& tube : tubes) {
+                    if(tube.second.building1 == pad.id || tube.second.building2 == pad.id) {
+                        occupancy++;
+                    }
+                }
+                if(occupancy > 4) continue;
+            }
+
+            for(pii& astronaut : pad.astronauts) {
+                if(resources < COSTS[POD]) break;
+
+                // check if pad already has connection to astronaut's destination
+                bool has_connection = false;
+                for(auto& [id, tube] : tubes) {
+                    if((tube.building1 == pad.id && buildings[tube.building2].type == astronaut.first) ||
+                        (tube.building2 == pad.id && buildings[tube.building1].type == astronaut.first)) {
+                        has_connection = true;
+                        break;
+                    }
+                }
+                if(has_connection) continue;
+                for(auto& [id, teleporter] : teleporters) {
+                    if(teleporter.building_from == pad.id && buildings[teleporter.building_to].type == astronaut.first) {
+                        has_connection = true;
+                        break;
+                    }
+                }
+                if(has_connection) continue;
+
+
+                // find closest building of astronaut's type
+                int closest_building_id = -1;
+                for(auto& [id, building] : buildings) {
+                    if(building.type != astronaut.first) 
+                        continue;
+
+                    // check if building is occupied <=> has more than 4 tubes
+                    int occupation = 0;
+                    for(auto& tube : tubes) {
+                        if(tube.second.building2 == building.id || tube.second.building1 == building.id) {
+                            // tube already exists
+                            if(tube.second.building1 == pad.id || tube.second.building2 == pad.id) {
+                                occupation = 5;
+                            }
+                            occupation++;
+                        }
+                    }
+                    if(occupation > 4) continue;
+
+                    // check if building is closer than the current closest
+                    if(closest_building_id != -1 && 
+                    pad.pos.dist(building.pos) > pad.pos.dist(buildings[closest_building_id].pos))
+                        continue;
+
+                    // check if tube collides with some other tube
+                    bool collides = false;
+                    Segment new_tube_segment = {pad.pos, building.pos};
+                    for(auto& [id, tube] : tubes) {
+                        // if tube ends in same place (only one side) then it wont collide
+                        if(tube.building1 == pad.id || tube.building2 == building.id)
+                            continue;
+
+                        //! TODO very not nice: assumes that pad is first building in any tube
+                        Segment tube_segment = {pads[tube.building1].pos, buildings[tube.building2].pos};
+
+
+                        if(tube_segment.intersects_segment(new_tube_segment)) {
+                            collides = true;
+                            break;
+                        }
+                    }
+                    if(collides) continue;
+
+                    // check if tube collides with some other building
+                    collides = false;
+                    for(auto& [id, building2] : buildings) {
+                        if(building2.id == building.id) continue;
+
+                        Segment new_tube_segment = {pad.pos, building.pos};
+
+
+                        if(new_tube_segment.is_point_on_segment(building2.pos)) {
+                            collides = true;
+                            break;
+                        }
+                    }
+                    if(collides) continue;
+                    for(auto& [id, pad2] : pads) {
+                        if(pad2.id == pad.id) continue;
+
+                        Segment new_tube_segment = {pad.pos, building.pos};
+
+                        if(new_tube_segment.is_point_on_segment(pad2.pos)) {
+                            collides = true;
+                            break;
+                        }
+                    }
+                    if(collides) continue;
+
+                    closest_building_id = building.id;
+                }
+
+
+                if(closest_building_id != -1) {
+                    int total_cost = COSTS[TUBE]*pad.pos.dist(buildings[closest_building_id].pos) + COSTS[POD];
+                    if(total_cost > resources) break;
+                    resources -= total_cost;
+                    
+                    moves.push_back({TUBE, {pad.id, closest_building_id}});
+                    tubes[tubes.size()*10] = {tubes.size()*10, pad.id, closest_building_id};
+
+                    if(resources < COSTS[POD]) break;
+                    moves.push_back({POD, {int(pods.size()), pad.id, closest_building_id}});
+                    pods[int(pods.size())] = {int(pods.size()), {pad.id, closest_building_id}};
+                }
+            }
+        }
+
+        return moves;
+    }
+
+    vector<Move> greedy_upgrade() {
+        // TODO
+        return {};
     }
 };
 
@@ -324,8 +483,8 @@ void parse_input(Solution &sol) {
     vector<LandingPad> pads;
     for (int i = 0; i < num_new_buildings; i++) {
         int type, building_id;
-        pii pos;
-        cin >> type >> building_id >> pos.first >> pos.second;
+        Point pos;
+        cin >> type >> building_id >> pos.x >> pos.y;
         if(type == 0) { // landing pad
             int num_astronauts;
             cin >> num_astronauts;
@@ -354,7 +513,7 @@ void parse_input(Solution &sol) {
         }
     }
 
-    sol = Solution(resources, tubes, teleporters, pods, sol.buildings, sol.pads);
+    sol.recreate_info(resources, tubes, teleporters, pods);
     sol.add_buildings(buildings, pads);
 }
 
@@ -363,18 +522,28 @@ int main() {
     Solution sol;
 
     while (1) {
-        cerr << "new loop, before parsing input\n";
         parse_input(sol);
 
-        vector<Move> moves = sol.greedy_teleporters();
+        vector<Move> moves;
 
+        // greedy tubes
+        moves = sol.greedy_tubes();
         for(Move& move : moves) {
-            cout << "TELEPORT " << move.args[0] << " " << move.args[1] << ';';
+            cout << MOVE_NAMES[move.type] << " ";
+            for(int arg : move.args) {
+                cout << arg << " ";
+            }
+            cout << ";";
         }
-        if(moves.empty()) {
-            cout << "WAIT;";
+        
+        // greedy teleporters
+        moves = sol.greedy_teleporters();
+        for(Move& move : moves) {
+            cout << MOVE_NAMES[move.type] << " ";
+            cout << move.args[0] << " " << move.args[1] << ';';
         }
-        cout << endl;
+
+        cout << "WAIT" << endl;
     }
 }
 

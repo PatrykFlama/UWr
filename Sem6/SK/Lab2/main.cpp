23baedd64362efb452cb4516a7456beaf011da59
Patryk Flama <51850536+PatrykFlama@users.noreply.github.com>
2025-04-10 17:08:11 +0200

    soooo much content

---
+++ Sem6/SK/Lab2/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#include <cstdlib>
+#include <cerrno>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <poll.h>
+
+using namespace std;
+
+#define DEBUG 0     // debug stage; 0 = none, verbose = 3, minimal >= 4
+#define dprintf if(DEBUG) printf
+
+
+
+

1aaa817a5597c8673c8b448aa82e8828525b169f
PatrykFlama <patrykflama@interia.pl>
2025-10-19 13:07:45 +0200

    zad1 kinda done

--- Sem7/Embedded/List2/zad1/zad1.c
+++ Sem7/Embedded/List2/zad1/zad1.c
@@ -1,9 +1,12 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/atomic.h>
 
-volatile unsigned long timer1_millis = 0;
+// https://gist.github.com/adnbr/2439125
+volatile unsigned long timer_millis = 0;
 ISR (TIMER1_COMPA_vect)
 {
-    timer1_millis++;
+    timer_millis++;
 }
 
 #define LED PB5
@@ -14,12 +17,12 @@ ISR (TIMER1_COMPA_vect)
 #define BTN_PIN PINB
 #define BTN_PORT PORTB
 
-const int BUFOR_SIZE = 100;
-const time_t TIME_OFFSET = 1000;   // milliseconds
+#define BUFOR_SIZE 16
+#define TIME_OFFSET 1000   // milliseconds
 
 struct State
 {
-    bool btn_state;
+    int btn_state;
     int timestamp;
 };
 
@@ -32,7 +35,7 @@ int main() {
     BTN_PORT |= _BV(BTN);
     LED_DDR |= _BV(LED);
 
-    long start = timer1_millis;
+    long start = timer_millis;
     int last_state = 0;
     int bufor_ptr = 0;
 
@@ -43,17 +46,17 @@ int main() {
             last_state = curr_state;
 
             if (bufor_begin == bufor_ptr) {
-                start = timer1_millis;
-                bufor[bufor_ptr] = {curr_state, start + TIME_OFFSET};
+                start = timer_millis;
+                bufor[bufor_ptr] = (struct State){curr_state, start + TIME_OFFSET};
                 bufor_ptr = (bufor_ptr + 1) % BUFOR_SIZE;
             } else {
-                bufor[bufor_ptr] = {curr_state, timer1_millis + TIME_OFFSET};
+                bufor[bufor_ptr] = (struct State){curr_state, timer_millis + TIME_OFFSET};
                 bufor_ptr = (bufor_ptr + 1) % BUFOR_SIZE;
             }
         }
 
         // update LED
-        if (bufor[bufor_begin].timestamp <= timer1_millis) {
+        if (bufor[bufor_begin].timestamp <= timer_millis) {
             if (bufor[bufor_begin].btn_state)
                 LED_PORT &= ~_BV(LED);
             else

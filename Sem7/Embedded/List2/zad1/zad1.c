c42315e5011aa74b64600468821508f0480f5ab8
PatrykFlama <patrykflama@interia.pl>
2025-10-19 14:14:52 +0200

    cleanup

--- Sem7/Embedded/List2/zad1/zad1.c
+++ Sem7/Embedded/List2/zad1/zad1.c
@@ -2,6 +2,9 @@
 #include <avr/interrupt.h>
 #include <util/atomic.h>
 
+// Calculate the value needed for the CTC match value in OCR1A (for 1ms)
+#define CTC_MATCH_OVERFLOW ((F_CPU / 1000) / 64)
+
 // https://gist.github.com/adnbr/2439125
 volatile unsigned long timer_millis = 0;
 ISR (TIMER1_COMPA_vect)
@@ -9,15 +12,27 @@ ISR (TIMER1_COMPA_vect)
     timer_millis++;
 }
 
-// Initialize Timer1 to generate interrupt every 1 ms (assumes 16MHz CPU)
-static void timer1_init_ms(void)
+unsigned long millis(void)
 {
-    // CTC mode (WGM12 = 1)
-    TCCR1B |= (1 << WGM12);
-    // prescaler 64 -> 16MHz/64 = 250kHz -> 250 ticks = 1ms
-    TCCR1B |= (1 << CS11) | (1 << CS10);
-    OCR1A = 250 - 1;
-    // enable compare A interrupt
+    unsigned long m;
+    ATOMIC_BLOCK(ATOMIC_FORCEON) {
+        m = timer_millis;
+    }
+    return m;
+}
+
+// Initialize Timer to generate interrupt every 1 ms (CTC mode)
+static void timer_init_ms(void)
+{
+    // CTC mode, Clock/64 (WGM12 and CS11:CS10 = 1:1)
+    TCCR1B |= (1 << WGM12) | (1 << CS11) | (1 << CS10);
+
+    // Load OCR1A high and low bytes
+    uint16_t ocr = (uint16_t)(CTC_MATCH_OVERFLOW - 1);
+    OCR1AH = (uint8_t)(ocr >> 8);
+    OCR1AL = (uint8_t)(ocr & 0xFF);
+
+    // Enable the compare match interrupt
     TIMSK1 |= (1 << OCIE1A);
 }
 
@@ -50,7 +65,7 @@ int main() {
 
     // initialize timer
     cli();
-    timer1_init_ms();
+    timer_init_ms();
     sei();
 
     uint8_t last_state = (BTN_PIN & _BV(BTN)) ? 1 : 0;

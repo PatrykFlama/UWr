0853b338ad5229a8bf1b39a6fe4e7c9d8556dd35
PatrykFlama <patrykflama@gmail.com>
2025-10-23 18:50:04 +0200

    mkey

---
+++ Sem7/LM/shell.nix
@@ -0,0 +1,14 @@
+#!/usr/bin/env nix-shell
+{ pkgs ? import <nixpkgs> { } }:
+(
+  let base = pkgs.appimageTools.defaultFhsEnvArgs; in
+  pkgs.buildFHSEnv (base // {
+    name = "FHS";
+    targetPkgs = pkgs: (with pkgs; [
+      gcc glibc zlib
+    ]);
+    runScript = "bash";
+    extraOutputsToInstall = [ "dev" ];
+  })
+).env
+
